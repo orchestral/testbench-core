2a5bafeaac992c4c89da371f5bdd76cb99abb837
crynobone <crynobone@gmail.com>
2017-08-19 11:07:38 +0800

    Release v3.4.1
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -17,7 +17,6 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
 use Orchestra\Testbench\Contracts\TestCase as TestCaseContract;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithConsole;
-use Illuminate\Foundation\Testing\Concerns\InteractsWithSession;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
 use Illuminate\Foundation\Testing\Concerns\MocksApplicationServices;
@@ -31,7 +30,6 @@ abstract class TestCase extends BaseTestCase implements TestCaseContract
         InteractsWithAuthentication,
         InteractsWithConsole,
         InteractsWithDatabase,
-        InteractsWithSession,
         MocksApplicationServices,
         WithFactories,
         WithLaravelMigrations,
