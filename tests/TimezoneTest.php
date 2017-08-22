63ee47f3d4f3359f6d4622316e89d3b135745b9c
crynobone <crynobone@gmail.com>
2017-08-22 17:45:00 +0800

    Ensure timezone can be replaced.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/TimezoneTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Orchestra\Testbench\Tests;
+
+use Carbon\Carbon;
+use Illuminate\Routing\Router;
+use Orchestra\Testbench\TestCase;
+
+class TimezoneTest extends TestCase
+{
+    /**
+     * Get application timezone.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return string|null
+     */
+    protected function getApplicationTimezone($app)
+    {
+        return 'Asia/Kuala_Lumpur';
+    }
+
+    /**
+     * @test
+     */
+    public function testOverridingTimezone()
+    {
+        $this->assertEquals('Asia/Kuala_Lumpur', Carbon::now()->timezoneName);
+    }
+}
