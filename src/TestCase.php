cb8dc292ded3bac8208bad12540a4fc528482082
scrubmx <scrub.mx@gmail.com>
2017-09-28 23:00:14 +0800

    Add mockery expectations to the assertion count
    Fixes an issue where tests that use mockery expectations that don't have
    any assertions are marked as risky.
    
    mockery/mockery#376
    GrahamCampbell/Laravel-TestBench@981289c
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -163,6 +163,10 @@ abstract class TestCase extends BaseTestCase implements TestCaseContract
         }
 
         if (class_exists('Mockery')) {
+            if ($container = Mockery::getContainer()) {
+                $this->addToAssertionCount($container->mockery_getExpectationCount());
+            }
+
             Mockery::close();
         }
 
