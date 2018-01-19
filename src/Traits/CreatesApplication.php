3cacf6080164449b5cabdefc98c8c5c13edd4d20
Mior Muhammad Zaki <crynobone@gmail.com>
2018-01-19 22:41:24 +0800

    Update deprecation note.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Traits/CreatesApplication.php
+++ src/Traits/CreatesApplication.php
@@ -2,8 +2,12 @@
 
 namespace Orchestra\Testbench\Traits;
 
-use Orchestra\Testbench\Concerns\CreatesApplication;
+use Orchestra\Testbench\Concerns\CreatesApplication as Concern;
 
+/**
+ * @deprecated v3.6.0
+ * @see \Orchestra\Testbench\Concerns\CreatesApplication
+ */
 trait CreatesApplication
 {
     use Concern;
