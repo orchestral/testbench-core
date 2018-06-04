e2266f71ccd6bb7932b6b3f58db4a193588b8610
Mior Muhammad Zaki <crynobone@gmail.com>
2018-06-05 06:37:53 +0800

    Update Laravel 5.7 skeleton.
    Updated to laravel/laravel@1dfa2851fabe2b947ad4d9db67f7e15e8af69597
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -120,6 +120,13 @@ return [
             'database' => env('REDIS_DB', 0),
         ],
 
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 0),
+        ],
+
     ],
 
 ];
