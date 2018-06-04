9eb8f589021f1672c8c5adf9c886348a02951494
Mior Muhammad Zaki <crynobone@gmail.com>
2018-06-05 06:37:01 +0800

    Add redis password.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -115,6 +115,7 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
         ],
