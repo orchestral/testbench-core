490a9788fb7ee4741e7c1597b5602c09e503b1a5
Dmitry <coder1994@gmail.com>
2019-02-27 15:12:35 +0300

    Revert RedirectIfAuthenticated to Laravel's one
    https://github.com/laravel/laravel/blob/master/app/Http/Middleware/RedirectIfAuthenticated.php

--- src/Http/Middleware/RedirectIfAuthenticated.php
+++ src/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,16 +12,14 @@ class RedirectIfAuthenticated
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guards
-     *
+     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guards = null)
+    public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
-
         return $next($request);
     }
 }
