21b90837dc0a81dbc26ab0003e785eeec4b4d509
Dmitry <coder1994@gmail.com>
2019-02-27 15:18:30 +0300

    Update RedirectIfAuthenticated.php

--- src/Http/Middleware/RedirectIfAuthenticated.php
+++ src/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,6 +5,7 @@ namespace Orchestra\Testbench\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
+
 class RedirectIfAuthenticated
 {
     /**
@@ -20,6 +21,7 @@ class RedirectIfAuthenticated
         if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
+
         return $next($request);
     }
 }
