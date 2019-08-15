9d26357eca8c9891b9875530e31fdac175a8e13d
Mior Muhammad Zaki <crynobone@gmail.com>
2019-08-15 09:36:40 +0800

    Add refresh database test.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Databases/RefreshDatabaseTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Orchestra\Testbench\Tests\Databases;
+
+use Orchestra\Testbench\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RefreshDatabaseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('migrate', ['--database' => 'testing']);
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+    }
+
+    /**
+     * Get package providers.  At a minimum this is the package being tested, but also
+     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
+     * In a normal app environment these would be added to the 'providers' array in
+     * the config/app.php file.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Orchestra\Testbench\Tests\Stubs\Providers\ServiceProvider::class,
+            //'Cartalyst\Sentry\SentryServiceProvider',
+            //'YourProject\YourPackage\YourPackageServiceProvider',
+        ];
+    }
+
+    /**
+     * Get package aliases.  In a normal app environment these would be added to
+     * the 'aliases' array in the config/app.php file.  If your package exposes an
+     * aliased facade, you should add the alias here, along with aliases for
+     * facades upon which your package depends, e.g. Cartalyst/Sentry.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            //'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+            //'YourPackage' => 'YourProject\YourPackage\Facades\YourPackage',
+        ];
+    }
+
+    /** @test */
+    public function it_runs_the_migrations()
+    {
+        $users = \DB::table('testbench_users')->where('id', '=', 1)->first();
+
+        $this->assertEquals('hello@orchestraplatform.com', $users->email);
+        $this->assertTrue(\Hash::check('123', $users->password));
+
+        $this->assertEquals([
+            'id',
+            'email',
+            'password',
+            'created_at',
+            'updated_at',
+        ], \Schema::getColumnListing('testbench_users'));
+    }
+}
