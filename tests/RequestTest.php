2d52d9a5eeb01e69570b667eee2d4eab874c85b2
Mior Muhammad Zaki <crynobone@gmail.com>
2019-10-09 20:29:12 +0800

    Add tests to interact with request information.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -7,6 +7,20 @@ use Orchestra\Testbench\TestCase;
 class RequestTest extends TestCase
 {
     /** @test */
+    public function it_can_get_request_information()
+    {
+        $this->app['router']->get('hello', ['uses' => function () {
+            return 'hello world';
+        }]);
+
+        $this->call('GET', 'hello?foo=bar');
+
+        $this->assertSame('http://localhost/hello?foo=bar', url()->full());
+        $this->assertSame('http://localhost/hello', url()->current());
+        $this->assertSame(['foo' => 'bar'], request()->all());
+    }
+
+    /** @test */
     public function it_flashes_request_values()
     {
         $oldValue = 'test-old-value';
