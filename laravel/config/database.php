f24f1490598d3949b525313569188ba21f0cb1c7
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-18 11:00:10 +0800

    Update skeleton.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -46,7 +46,7 @@ return [
             'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', false),
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
