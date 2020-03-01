130be3e29b18410adf98437fe63d1c89c4d9ab0e
Mior Muhammad Zaki <crynobone@gmail.com>
2020-03-02 07:03:32 +0800

    Abstract code to test runner.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commands/TestRunnerCommand.php
+++ src/Console/Commands/TestRunnerCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Concerns\HasParameters;
 use Illuminate\Console\Concerns\InteractsWithIO;
 use Illuminate\Console\OutputStyle;
 use Illuminate\Support\Str;
+use Orchestra\Testbench\TestRunner;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -39,16 +40,6 @@ class TestRunnerCommand extends Command
      * @var string
      */
     protected $workingPath;
-
-    /**
-     * The arguments to be used while calling phpunit.
-     *
-     * @var array
-     */
-    protected $arguments = [
-        '--printer',
-        'NunoMaduro\Collision\Adapters\Phpunit\Printer',
-    ];
 
     /**
      * Create a new command instance.
@@ -102,64 +93,11 @@ class TestRunnerCommand extends Command
     {
         $options = \array_slice($_SERVER['argv'], $this->option('without-tty') ? 3 : 2);
 
-        $process = (new Process(\array_merge(
-            $this->binary(), \array_merge(
-                $this->arguments,
-                $this->phpunitArguments($options)
-            )
-        )))->setTimeout(null);
+        $runner = new TestRunner(
+            $this->output, $this->workingPath, $this->option('without-tty')
+        );
 
-        try {
-            $process->setTty(! $this->option('without-tty'));
-        } catch (RuntimeException $e) {
-            $this->output->writeln('Warning: '.$e->getMessage());
-        }
-
-        try {
-            return $process->run(function ($type, $line) {
-                $this->output->write($line);
-            });
-        } catch (ProcessSignaledException $e) {
-            if (\extension_loaded('pcntl') && $e->getSignal() !== SIGINT) {
-                throw $e;
-            }
-        }
-
-        return 0;
-    }
-
-    /**
-     * Get the PHP binary to execute.
-     *
-     * @return array
-     */
-    protected function binary()
-    {
-        if ('phpdbg' === PHP_SAPI) {
-            return [PHP_BINARY, '-qrr', 'vendor/phpunit/phpunit/phpunit'];
-        }
-
-        return [PHP_BINARY, 'vendor/phpunit/phpunit/phpunit'];
-    }
-
-    /**
-     * Get the array of arguments for running PHPUnit.
-     *
-     * @param  array  $options
-     *
-     * @return array
-     */
-    protected function phpunitArguments($options)
-    {
-        $options = \array_values(\array_filter($options, static function ($option) {
-            return ! Str::startsWith($option, '--env=');
-        }));
-
-        if (! \file_exists($file = "{$this->workingPath}/phpunit.xml")) {
-            $file = "{$this->workingPath}/phpunit.xml.dist";
-        }
-
-        return \array_merge(['-c', $file], $options);
+        return $runner($options);
     }
 
     /**
---
+++ src/TestRunner.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Orchestra\Testbench;
+
+use Illuminate\Support\Str;
+use RuntimeException;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Exception\ProcessSignaledException;
+use Symfony\Component\Process\Process;
+
+/**
+ * PoC, this will be PRed to Collision.
+ */
+class TestRunner
+{
+    /**
+     * The output interface implementation.
+     *
+     * @var \Symfony\Component\Console\Style\SymfonyStyle
+     */
+    protected $output;
+
+    /**
+     * Test runner working path.
+     *
+     * @var string
+     */
+    protected $workingPath;
+
+    /**
+     * Disable output to TTY.
+     *
+     * @var bool
+     */
+    protected $withoutTty;
+
+    /**
+     * The arguments to be used while calling phpunit.
+     *
+     * @var array
+     */
+    protected $arguments = [
+        '--printer',
+        'NunoMaduro\Collision\Adapters\Phpunit\Printer',
+    ];
+
+    /**
+     * Creates a new instance of the TestRunner.
+     *
+     * @param \Symfony\Component\Console\Style\SymfonyStyle $output
+     * @param string $workingPath
+     * @param bool $withoutTty
+     */
+    public function __construct(SymfonyStyle $output, string $workingPath, bool $withoutTty = false)
+    {
+        $this->output = $output;
+        $this->workingPath = $workingPath;
+        $this->withoutTty = $withoutTty;
+    }
+
+    /**
+     * Handle the test runner.
+     *
+     * @param  array  $options
+     *
+     * @return int
+     */
+    public function __invoke(array $options): int
+    {
+        $process = (new Process(array_merge(
+            $this->binary(), array_merge(
+                $this->arguments,
+                $this->phpunitArguments($options)
+            )
+        )))->setTimeout(null);
+
+        try {
+            $process->setTty(! $this->withoutTty);
+        } catch (RuntimeException $e) {
+            $this->output->writeln('Warning: '.$e->getMessage());
+        }
+
+        try {
+            return $process->run(function ($type, $line) {
+                $this->output->write($line);
+            });
+        } catch (ProcessSignaledException $e) {
+            if (extension_loaded('pcntl') && $e->getSignal() !== SIGINT) {
+                throw $e;
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * Get the PHP binary to execute.
+     *
+     * @return array
+     */
+    protected function binary(): array
+    {
+        if ('phpdbg' === PHP_SAPI) {
+            return [PHP_BINARY, '-qrr', 'vendor/phpunit/phpunit/phpunit'];
+        }
+
+        return [PHP_BINARY, 'vendor/phpunit/phpunit/phpunit'];
+    }
+
+    /**
+     * Get the array of arguments for running PHPUnit.
+     *
+     * @param  array  $options
+     *
+     * @return array
+     */
+    protected function phpunitArguments(array $options): array
+    {
+        $options = array_values(array_filter($options, function ($option) {
+            return ! Str::startsWith($option, '--env=');
+        }));
+
+        if (! file_exists($file = "{$this->workingPath}/phpunit.xml")) {
+            $file = "{$this->workingPath}/phpunit.xml.dist";
+        }
+
+        return array_merge(['-c', $file], $options);
+    }
+}
