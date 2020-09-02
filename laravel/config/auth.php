388ef68fe9a2c94063b997fd7e36306e05e95aab
Mior Muhammad Zaki <crynobone@gmail.com>
2020-09-02 22:27:14 +0800

    Ship default UserFactory.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/auth.php
+++ laravel/config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => Illuminate\Foundation\Auth\User::class,
         ],
 
         // 'users' => [
