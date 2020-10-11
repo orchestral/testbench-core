8852a3df765a8b97f282edffeba388265a702b1d
Mior Muhammad Zaki <crynobone@gmail.com>
2020-10-11 15:01:46 +0800

    Add \ prefix.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commander.php
+++ src/Console/Commander.php
@@ -91,7 +91,7 @@ class Commander
     protected function createDotenv()
     {
         (new Dotenv(
-            new StringStore(implode("\n", $this->config['env'] ?? [])),
+            new StringStore(\implode("\n", $this->config['env'] ?? [])),
             new Parser(),
             new Loader(),
             Env::getRepository()
