2dbdba10830d9b071eb931ba80979919c3b2f8e4
Mior Muhammad Zaki <crynobone@gmail.com>
2020-12-04 12:04:51 +0800

    Add ability to cache routes.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commander.php
+++ src/Console/Commander.php
@@ -57,7 +57,7 @@ class Commander
      */
     public function handle()
     {
-        $laravel = $this->createApplication();
+        $laravel = $this->laravel();
         $kernel = $laravel->make(ConsoleKernel::class);
 
         $status = $kernel->handle(
@@ -67,6 +67,20 @@ class Commander
         $kernel->terminate($input, $status);
 
         exit($status);
+    }
+
+    /**
+     * Create Laravel application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function laravel()
+    {
+        if (! $this->app) {
+            $this->app = $this->createApplication();
+        }
+
+        return $this->app;
     }
 
     /**
