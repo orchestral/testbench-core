e7c59b88f667ef67f5b6762fbfbd24484d97aad4
Mior Muhammad Zaki <crynobone@gmail.com>
2021-01-17 21:51:38 +0800

    Add Parallel setup and teardown.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/Testing.php
+++ src/Concerns/Testing.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\WithoutEvents;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\ParallelTesting;
 use Mockery;
 use Throwable;
 
@@ -76,6 +77,8 @@ trait Testing
     {
         if (! $this->app) {
             $this->refreshApplication();
+
+            ParallelTesting::callSetUpTestCaseCallbacks($this);
         }
 
         foreach ($this->afterApplicationRefreshedCallbacks as $callback) {
@@ -104,6 +107,8 @@ trait Testing
     {
         if ($this->app) {
             $this->callBeforeApplicationDestroyedCallbacks();
+
+            ParallelTesting::callTearDownTestCaseCallbacks($this);
 
             $this->app->flush();
 
