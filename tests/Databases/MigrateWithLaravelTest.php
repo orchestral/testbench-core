19164c5c4b04600bbb749c24d378be3715cc2c95
Mior Muhammad Zaki <crynobone@gmail.com>
2021-03-28 11:34:27 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Databases/MigrateWithLaravelTest.php
+++ tests/Databases/MigrateWithLaravelTest.php
@@ -20,52 +20,31 @@ class MigrateWithLaravelTest extends TestCase
     }
 
     /**
-     * Define database migrations.
-     *
-     * @return void
+     * @test
+     * @define-db loadApplicationMigrations
      */
-    protected function defineDatabaseMigrations()
+    public function it_loads_the_migrations()
     {
-        $this->loadLaravelMigrations(['--database' => 'testing']);
+        $now = Carbon::now();
+
+        \DB::table('users')->insert([
+            'name' => 'Orchestra',
+            'email' => 'hello@orchestraplatform.com',
+            'password' => \Hash::make('456'),
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        $users = \DB::table('users')->where('id', '=', 1)->first();
+
+        $this->assertEquals('hello@orchestraplatform.com', $users->email);
+        $this->assertTrue(\Hash::check('456', $users->password));
     }
 
     /**
-     * Get package providers.  At a minimum this is the package being tested, but also
-     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
-     * In a normal app environment these would be added to the 'providers' array in
-     * the config/app.php file.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
+     * @test
+     * @define-db runApplicationMigrations
      */
-    protected function getPackageProviders($app)
-    {
-        return [
-            //'Cartalyst\Sentry\SentryServiceProvider',
-            //'YourProject\YourPackage\YourPackageServiceProvider',
-        ];
-    }
-
-    /**
-     * Get package aliases.  In a normal app environment these would be added to
-     * the 'aliases' array in the config/app.php file.  If your package exposes an
-     * aliased facade, you should add the alias here, along with aliases for
-     * facades upon which your package depends, e.g. Cartalyst/Sentry.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return [
-            //'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
-            //'YourPackage' => 'YourProject\YourPackage\Facades\YourPackage',
-        ];
-    }
-
-    /** @test */
     public function it_runs_the_migrations()
     {
         $now = Carbon::now();
@@ -82,5 +61,28 @@ class MigrateWithLaravelTest extends TestCase
 
         $this->assertEquals('hello@orchestraplatform.com', $users->email);
         $this->assertTrue(\Hash::check('456', $users->password));
+    }
+
+    public function loadApplicationMigrations()
+    {
+        $this->loadLaravelMigrations(['--database' => 'testing']);
+    }
+
+    public function runApplicationMigrations()
+    {
+        $this->callAfterResolving('migrator', function ($migrator) {
+            $migrator->path(base_path('migrations'));
+        });
+
+        $this->runLaravelMigrations(['--database' => 'testing']);
+    }
+
+    protected function callAfterResolving($name, $callback)
+    {
+        $this->app->afterResolving($name, $callback);
+
+        if ($this->app->resolved($name)) {
+            $callback($this->app->make($name), $this->app);
+        }
     }
 }
