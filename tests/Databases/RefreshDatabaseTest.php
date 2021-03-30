0834673a6ca8c483671e4f007a904df4fbc7ae5b
Mior Muhammad Zaki <crynobone@gmail.com>
2021-03-30 20:08:13 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Databases/RefreshDatabaseTest.php
+++ tests/Databases/RefreshDatabaseTest.php
@@ -32,10 +32,7 @@ class RefreshDatabaseTest extends TestCase
     }
 
     /**
-     * Get package providers.  At a minimum this is the package being tested, but also
-     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
-     * In a normal app environment these would be added to the 'providers' array in
-     * the config/app.php file.
+     * Get package providers.
      *
      * @param  \Illuminate\Foundation\Application  $app
      *
@@ -45,26 +42,6 @@ class RefreshDatabaseTest extends TestCase
     {
         return [
             \Orchestra\Testbench\Tests\Fixtures\Providers\ServiceProvider::class,
-            //'Cartalyst\Sentry\SentryServiceProvider',
-            //'YourProject\YourPackage\YourPackageServiceProvider',
-        ];
-    }
-
-    /**
-     * Get package aliases.  In a normal app environment these would be added to
-     * the 'aliases' array in the config/app.php file.  If your package exposes an
-     * aliased facade, you should add the alias here, along with aliases for
-     * facades upon which your package depends, e.g. Cartalyst/Sentry.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return [
-            //'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
-            //'YourPackage' => 'YourProject\YourPackage\Facades\YourPackage',
         ];
     }
 
