92f8b32db2a486f802c15174da74693b4132ad1e
Mior Muhammad Zaki <crynobone@gmail.com>
2021-05-26 08:21:04 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -25,6 +25,13 @@ class Application
     protected $resolvingCallback;
 
     /**
+     * Load Environment variables.
+     *
+     * @var bool
+     */
+    protected $loadEnvironmentVariables = false;
+
+    /**
      * Create new application resolver.
      *
      * @param  string  $basePath
@@ -41,12 +48,28 @@ class Application
      *
      * @param  string  $basePath
      * @param  callable|null  $resolvingCallback
+     * @param  array  $options
      *
      * @return \Illuminate\Foundation\Application
      */
-    public static function create(string $basePath, ?callable $resolvingCallback = null)
+    public static function create(string $basePath, ?callable $resolvingCallback = null, array $options = [])
     {
-        return (new static($basePath, $resolvingCallback))->createApplication();
+        return (new static($basePath, $resolvingCallback))->configure($options)->createApplication();
+    }
+
+    /**
+     * Configure the application options.
+     *
+     * @param  array  $options
+     * @return $this
+     */
+    public function configure(array $options)
+    {
+        if (isset($options['load_environment_variables']) && \is_bool($options['load_environment_variables'])) {
+            $this->loadEnvironmentVariables = $options['load_environment_variables'];
+        }
+
+        return $this;
     }
 
     /**
