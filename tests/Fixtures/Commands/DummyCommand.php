07e03cddade75b59292a64da855df7bafc582d48
Mior Muhammad Zaki <crynobone@gmail.com>
2021-10-08 08:55:32 +0800

    Add tests against #314
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Fixtures/Commands/DummyCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Orchestra\Testbench\Tests\Fixtures\Commands;
+
+use Illuminate\Console\Command;
+
+class DummyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sample:command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sample command';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('It works!');
+
+        return 0;
+    }
+}
