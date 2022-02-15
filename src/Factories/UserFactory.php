8de44a01a53ee2c43041529f13ba2e3b4c3bea7b
Mior Muhammad Zaki <crynobone@gmail.com>
2022-02-16 07:49:56 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Factories/UserFactory.php
+++ src/Factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function definition()
     {
