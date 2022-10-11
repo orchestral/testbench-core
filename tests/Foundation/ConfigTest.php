a6e414bc897e0e57a67b53092c442c0ad0abd844
Mior Muhammad Zaki <crynobone@gmail.com>
2022-10-11 11:05:51 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Foundation/ConfigTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Orchestra\Testbench\Tests\Foundation;
+
+use Orchestra\Testbench\Foundation\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /** @test */
+    public function it_can_load_configuration_file()
+    {
+        $config = Config::loadFromYaml(workingPath: __DIR__.'/stubs/');
+
+        $this->assertNull($config['laravel']);
+        $this->assertSame([], $config['env']);
+        $this->assertSame([
+            'Orchestra\Testbench\Foundation\TestbenchServiceProvider',
+        ], $config['providers']);
+        $this->assertSame([], $config['dont-discover']);
+    }
+}
