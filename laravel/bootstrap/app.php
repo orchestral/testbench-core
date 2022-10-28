0564bd044a40aae95537036abf21a896cf6304d8
Mior Muhammad Zaki <crynobone@gmail.com>
2022-10-28 19:09:03 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -1,15 +1,25 @@
 <?php
 
-use Orchestra\Testbench\Console\Commander;
+use Orchestra\Testbench\Foundation\Application;
+use Orchestra\Testbench\Foundation\Config;
+
+$workingPath = realpath(__DIR__.'/../');
 
 $APP_KEY = $_SERVER['APP_KEY'] ?? $_ENV['APP_KEY'] ?? 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF';
 $DB_CONNECTION = $_SERVER['DB_CONNECTION'] ?? $_ENV['DB_CONNECTION'] ?? 'testing';
 
-$config = ['env' => ['APP_KEY="'.$APP_KEY.'"', 'DB_CONNECTION="'.$DB_CONNECTION.'"'], 'providers' => []];
+$config = Config::loadFromYaml(
+    workingPath: $workingPath,
+    defaults: ['env' => ['APP_KEY="'.$APP_KEY.'"', 'DB_CONNECTION="'.$DB_CONNECTION.'"'], 'providers' => []]
+);
 
-$app = (new Commander($config, getcwd()))->laravel();
+/** @var \Illuminate\Foundation\Application $app */
+$app = Application::create(
+    basePath: $config['laravel'],
+    options: ['enables_package_discoveries' => true, 'extra' => $config->getExtraAttributes()],
+);
 
-unset($APP_KEY, $DB_CONNECTION, $config);
+unset($APP_KEY, $DB_CONNECTION, $workingPath, $config);
 
 $router = $app->make('router');
 
