9ea21a7221b2aa923190c4fc00c2611de88a8c78
Mior Muhammad Zaki <crynobone@gmail.com>
2022-11-07 19:39:58 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/PackageManifest.php
+++ src/Foundation/PackageManifest.php
@@ -30,7 +30,7 @@ class PackageManifest extends IlluminatePackageManifest
      * @param  \Illuminate\Filesystem\Filesystem  $files
      * @param  string  $basePath
      * @param  string  $manifestPath
-     * @param  object|null  $testbench
+     * @param  \Orchestra\Testbench\Contracts\TestCase|object|null  $testbench
      */
     public function __construct(Filesystem $files, $basePath, $manifestPath, $testbench = null)
     {
@@ -48,6 +48,7 @@ class PackageManifest extends IlluminatePackageManifest
      */
     public static function swap($app, $testbench = null)
     {
+        /** @var \Illuminate\Foundation\PackageManifest $base */
         $base = $app->make(IlluminatePackageManifest::class);
 
         $app->instance(
