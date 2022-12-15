ad9462cf79ca45daebd8102806c7dcf4206c7092
Mior Muhammad Zaki <crynobone@gmail.com>
2022-12-15 15:22:28 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -1,8 +1,6 @@
 <?php
 
-use function Orchestra\Testbench\default_environment_variables;
 use Orchestra\Testbench\Foundation\Application;
-use Orchestra\Testbench\Foundation\Bootstrap\LoadEnvironmentVariablesFromArray;
 use Orchestra\Testbench\Foundation\Config;
 
 /**
