ae5539178b85c06cfeca865a73fadeb617eede09
Mior Muhammad Zaki <crynobone@gmail.com>
2022-12-16 14:23:55 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,6 @@
 namespace Orchestra\Testbench;
 
 use Illuminate\Testing\PendingCommand;
-use Orchestra\Testbench\Foundation\Application;
 
 /**
  * Create Laravel application instance.
@@ -13,9 +12,9 @@ use Orchestra\Testbench\Foundation\Application;
  * @param  array{extra?: array{providers?: array, dont-discover?: array, env?: array}, load_environment_variables?: bool, enabled_package_discoveries?: bool}  $options
  * @return \Orchestra\Testbench\Foundation\Application
  */
-function container(?string $basePath = null, ?callable $resolvingCallback = null, array $options = []): Application
+function container(?string $basePath = null, ?callable $resolvingCallback = null, array $options = []): Foundation\Application
 {
-    return (new Application($basePath, $resolvingCallback))->configure($options);
+    return (new Foundation\Application($basePath, $resolvingCallback))->configure($options);
 }
 
 /**
