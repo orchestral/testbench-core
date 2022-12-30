aa3748fa5be7f8802b126160d6dfeff747997519
Mior Muhammad Zaki <crynobone@gmail.com>
2022-12-30 15:43:56 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/HandlesAnnotations.php
+++ src/Concerns/HandlesAnnotations.php
@@ -7,37 +7,50 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\Util\Annotation\Registry as PHPUnit9Registry;
 use ReflectionClass;
 
+/**
+ * @internal
+ */
 trait HandlesAnnotations
 {
     /**
-     * Parse test method annotations.
+     * Resolve PHPUnit method annotations.
      *
      * @phpunit-overrides
+     *
+     * @return \Illuminate\Support\Collection<string, mixed>
+     */
+    protected function resolvePhpUnitAnnotations(): Collection
+    {
+        $instance = new ReflectionClass($this);
+
+        if (! $this instanceof TestCase || $instance->isAnonymous()) {
+            return new Collection();
+        }
+
+        /** @var array<string, mixed> $annotations */
+        $annotations = rescue(
+            fn () => PHPUnit9Registry::getInstance()->forMethod($instance->getName(), $this->getName(false))->symbolAnnotations(),
+            [],
+            false
+        );
+
+        return Collection::make($annotations);
+    }
+
+    /**
+     * Parse test method annotations.
      *
      * @param  \Illuminate\Foundation\Application  $app
      * @param  string  $name
      */
     protected function parseTestMethodAnnotations($app, string $name): void
     {
-        $instance = new ReflectionClass($this);
-
-        if (! $this instanceof TestCase || $instance->isAnonymous()) {
-            return;
-        }
-
-        /** @var array<string, mixed> $annotations */
-        $annotations = rescue(
-            fn () => PHPUnit9Registry::getInstance()->forMethod(static::class, $this->getName(false))->symbolAnnotations(),
-            [],
-            false
-        );
-
-        Collection::make($annotations)
-            ->filter(fn ($actions, $key) => $key === $name)
-            ->each(function ($actions) use ($app) {
-                (new Collection($actions ?? []))
+        $this->resolvePhpUnitAnnotations()
+            ->filter(fn (array $actions, string $key) => $key === $name && ! empty($actions))
+            ->each(function (array $actions) use ($app) {
+                Collection::make($actions)
                     ->filter(fn ($method) => \is_string($method) && method_exists($this, $method))
-                    ->each(function ($method) use ($app) {
+                    ->each(function (string $method) use ($app) {
                         $this->{$method}($app);
                     });
             });
