8cafed40913c44fdcbe78ed5e73f4dd64aedf300
Mior Muhammad Zaki <crynobone@gmail.com>
2023-01-06 11:38:47 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/PhpDeprecationsTest.php
+++ tests/PhpDeprecationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Testbench\Tests;
 
+use ErrorException;
 use Illuminate\Support\Facades\Log;
 use Orchestra\Testbench\Exceptions\DeprecatedException;
 use Orchestra\Testbench\TestCase;
@@ -53,6 +54,21 @@ class PhpDeprecationsTest extends TestCase
     }
 
     /**
+     * @test
+     *
+     * @define-env defineSkippedConvertDeprecationsToExceptions
+     */
+    public function handle_php81_deprecations_using_laravel_exception_using_()
+    {
+        $this->withoutDeprecationHandling();
+
+        $this->expectException(ErrorException::class);
+        $this->expectExceptionMessage('zzz');
+
+        trigger_error('zzz', E_USER_DEPRECATED);
+    }
+
+    /**
      * Define environment.
      *
      * @param  \Illuminate\Foundation\Application  $app
@@ -62,6 +78,26 @@ class PhpDeprecationsTest extends TestCase
     {
         $value = $_ENV['TESTBENCH_CONVERT_DEPRECATIONS_TO_EXCEPTIONS'] ?? null;
         $_ENV['TESTBENCH_CONVERT_DEPRECATIONS_TO_EXCEPTIONS'] = true;
+
+        $this->beforeApplicationDestroyed(function () use ($value) {
+            if (\is_null($value)) {
+                unset($_ENV['TESTBENCH_CONVERT_DEPRECATIONS_TO_EXCEPTIONS']);
+            } else {
+                $_ENV['TESTBENCH_CONVERT_DEPRECATIONS_TO_EXCEPTIONS'] = $value;
+            }
+        });
+    }
+
+    /**
+     * Define environment.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineSkippedConvertDeprecationsToExceptions($app)
+    {
+        $value = $_ENV['TESTBENCH_CONVERT_DEPRECATIONS_TO_EXCEPTIONS'] ?? null;
+        $_ENV['TESTBENCH_CONVERT_DEPRECATIONS_TO_EXCEPTIONS'] = false;
 
         $this->beforeApplicationDestroyed(function () use ($value) {
             if (\is_null($value)) {
