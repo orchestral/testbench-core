48350224a42ac7311d3bf4b995bdd2232e4fac9c
Mior Muhammad Zaki <crynobone@gmail.com>
2023-02-08 10:35:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- sync.sh
+++ sync.sh
@@ -8,9 +8,9 @@ cp -rf skeleton/database/.gitignore laravel/database/.gitignore
 cp -rf skeleton/database/migrations/2014_10_12_000000_create_users_table.php laravel/migrations/2014_10_12_000000_testbench_create_users_table.php
 cp -rf skeleton/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php laravel/migrations/2014_10_12_100000_testbench_create_password_reset_tokens_table.php
 cp -rf skeleton/database/migrations/2019_08_19_000000_create_failed_jobs_table.php laravel/migrations/2019_08_19_000000_testbench_create_failed_jobs_table.php
-cp -rf skeleton/lang/en/*.php laravel/lang/en/
-cp -rf skeleton/resources/views/* laravel/resources/views/
+# cp -rf skeleton/lang/en/*.php laravel/lang/en/
 # cp -f skeleton/lang/*.json laravel/lang/
+cp -rf skeleton/resources/views/* laravel/resources/views/
 cp -rf skeleton/database/factories/*.php src/Factories/
 cp -rf vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php laravel/server.php
 cp -rf skeleton/public/index.php laravel/public/index.php
