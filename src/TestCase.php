884aae3d6fe28a38271fe4096ee416668c4ca289
Mior Muhammad Zaki <crynobone@gmail.com>
2023-03-23 08:43:37 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -94,6 +94,8 @@ abstract class TestCase extends PHPUnit implements Contracts\TestCase
 
         try {
             $result = parent::runTest();
+        } catch (Exceptions\DeprecatedException $error) {
+            throw $error;
         } catch (Throwable $error) {
             if (! \is_null(static::$latestResponse)) {
                 static::$latestResponse->transformNotSuccessfulException($error);
