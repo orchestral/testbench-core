cd29ab60bbfe540bbcc1caa46c7ec0f777a3ab46
Mior Muhammad Zaki <crynobone@gmail.com>
2023-03-28 07:54:38 +0800

    wip

---
+++ src/Concerns/WithInertia.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Orchestra\Testbench\Concerns;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\View\FileViewFinder;
+use Inertia\ServiceProvider;
+
+trait WithInertia
+{
+    /**
+     * Available Inertia page extensions.
+     * 
+     * @var array<int, string>
+     */
+    protected $inertiaPageExtensions = ['vue', 'js', 'jsx', 'ts', 'tsx', 'html', 'php'];
+
+    /** 
+     * Available Inertia page paths.
+     * 
+     * @var array<int, string>
+     */
+    protected $inertiaPagePaths = [
+        'resources/js/Pages', 
+        'resources/js/pages', 
+        'resources/ts/Pages', 
+        'resources/ts/pages', 
+        'resources/app/Pages', 
+        'resources/app/pages', 
+        'resources/views/pages',
+    ];
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setupWithInertia(): void
+    {
+        $this->app->register(ServiceProvider::class);
+    }
+
+    /**
+     * Configure Inertia for the test.
+     * 
+     * @param string  $viewLocation 
+     * @param array<string, string>  $namespaces
+     * @return void
+     */
+    public function loadInertia(string $viewLocation = 'resources/views', array $namespaces = []): void
+    {
+        View::addLocation($viewLocation);
+
+        $this->instance('inertia.testing.view-finder', function ($app) use ($namespaces) {
+            $finder = new FileViewFinder(
+                $app['files'],
+                array_merge($app['config']->get('inertia.testing.page_paths'), $this->inertiaPagePaths),
+                array_merge($app['config']->get('inertia.testing.page_extensions'), $this->inertiaPageExtensions)
+            );
+
+            foreach ($namespaces as $namespace => $namespacePagePath) {
+                $finder->addNamespace($namespace, $namespacePagePath);
+            }
+
+            return $finder;
+        });
+    }
+}
