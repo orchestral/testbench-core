1718b19bf74dcb4fb1c4fcd82d7f1d07439aa816
Mior Muhammad Zaki <crynobone@gmail.com>
2023-03-30 19:41:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Bootstrap/LoadEnvironmentVariablesFromArray.php
+++ src/Foundation/Bootstrap/LoadEnvironmentVariablesFromArray.php
@@ -37,8 +37,6 @@ final class LoadEnvironmentVariablesFromArray
      */
     public function bootstrap(Application $app): void
     {
-        Env::disablePutenv();
-
         $store = new StringStore(implode("\n", $this->environmentVariables));
         $parser = new Parser();
 
