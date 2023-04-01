f4eb69715ea2c35e6530248db1cf462b36e50935
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-01 09:44:03 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commander.php
+++ src/Console/Commander.php
@@ -13,7 +13,7 @@ use Orchestra\Testbench\Foundation\Bootstrap\LoadMigrationsFromArray;
 use Orchestra\Testbench\Foundation\Config;
 use Orchestra\Testbench\Foundation\TestbenchServiceProvider;
 use function Orchestra\Testbench\transform_relative_path;
-use Symfony\Component\Console\Application as SymfonyConsoleApplication;
+use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -193,7 +193,7 @@ class Commander
                 $handler->renderForConsole($output, $error);
             });
         } else {
-            (new SymfonyConsoleApplication)->renderThrowable($error, $output);
+            (new ConsoleApplication)->renderThrowable($error, $output);
         }
 
         return 1;
