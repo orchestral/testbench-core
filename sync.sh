a3769e0a0d03da0d1ef1a76f4a744955d492d767
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-01 16:40:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-composer create-project "laravel/laravel:9.x-dev" skeleton --no-scripts --no-plugins --quiet
-=======
 if [-f laravel/.env]; then
     rm laravel/.env
 fi
 
-composer create-project "laravel/laravel:10.x-dev" skeleton --no-scripts --no-plugins --quiet
->>>>>>> 597276d (Fixes configuration leaks via environment variables. (#107))
+composer create-project "laravel/laravel:9.x-dev" skeleton --no-scripts --no-plugins --quiet
 
 cp -f skeleton/.env.example laravel/
 cp -rf skeleton/config/*.php laravel/config/
