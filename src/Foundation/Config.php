9133312b079d6cddf87d72fee5f6b89f791f9393
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-02 17:58:27 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Config.php
+++ src/Foundation/Config.php
@@ -71,7 +71,9 @@ class Config extends Fluent
      */
     public function addProviders(array $providers)
     {
-        $this->attributes['providers'] = array_unique($this->attributes['providers'] + $providers);
+        $this->attributes['providers'] = array_unique(array_merge($this->attributes['providers'], $providers));
+
+        ray($this->attributes['providers']);
 
         return $this;
     }
--- tests/Foundation/ConfigTest.php
+++ tests/Foundation/ConfigTest.php
@@ -3,6 +3,7 @@
 namespace Orchestra\Testbench\Tests\Foundation;
 
 use Orchestra\Testbench\Foundation\Config;
+use Orchestra\Testbench\Tests\Fixtures\Providers\ChildServiceProvider;
 use PHPUnit\Framework\TestCase;
 
 class ConfigTest extends TestCase
@@ -31,5 +32,42 @@ class ConfigTest extends TestCase
             ],
             'dont-discover' => [],
         ], $config->getExtraAttributes());
+    }
+
+    /** @test */
+    public function it_can_add_additional_providers_to_configuration_file()
+    {
+        $config = Config::loadFromYaml(__DIR__.'/stubs/');
+
+        $this->assertSame([
+            'Orchestra\Testbench\Foundation\TestbenchServiceProvider',
+        ], $config['providers']);
+
+        $config->addProviders([
+            'Orchestra\Testbench\Tests\Fixtures\Providers\ChildServiceProvider',
+        ]);
+
+        $this->assertSame([
+            'Orchestra\Testbench\Foundation\TestbenchServiceProvider',
+            'Orchestra\Testbench\Tests\Fixtures\Providers\ChildServiceProvider',
+        ], $config['providers']);
+    }
+
+    /** @test */
+    public function it_cant_add_duplicated_providers_to_configuration_file()
+    {
+        $config = Config::loadFromYaml(__DIR__.'/stubs/');
+
+        $this->assertSame([
+            'Orchestra\Testbench\Foundation\TestbenchServiceProvider',
+        ], $config['providers']);
+
+        $config->addProviders([
+            'Orchestra\Testbench\Foundation\TestbenchServiceProvider',
+        ]);
+
+        $this->assertSame([
+            'Orchestra\Testbench\Foundation\TestbenchServiceProvider',
+        ], $config['providers']);
     }
 }
