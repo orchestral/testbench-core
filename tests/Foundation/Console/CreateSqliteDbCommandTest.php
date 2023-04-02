8388fdef4554fe312b105f7e900d923fb601dba4
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-02 23:37:25 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Foundation/Console/CreateSqliteDbCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Orchestra\Testbench\Tests\Foundation\Console;
+
+use Orchestra\Testbench\Foundation\TestbenchServiceProvider;
+use Orchestra\Testbench\TestCase;
+use Orchestra\Testbench\Tests\Concerns\Database\InteractsWithSqliteDatabase;
+
+class CreateSqliteDbCommandTest extends TestCase
+{
+    use InteractsWithSqliteDatabase;
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            TestbenchServiceProvider::class,
+        ];
+    }
+
+    /** @test */
+    public function it_can_generate_database_using_command()
+    {
+        $this->withoutSqliteDatabase(function () {
+            $this->assertFalse(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
+
+            $this->artisan('package:create-sqlite-db')
+                ->assertExitCode(0);
+
+            $this->assertTrue(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
+        });
+    }
+}
