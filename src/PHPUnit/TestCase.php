5b913e5d5cde17430d8e5b5d1e321129d74f7b5e
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-09 23:51:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/PHPUnit/TestCase.php
+++ src/PHPUnit/TestCase.php
@@ -5,7 +5,25 @@ namespace Orchestra\Testbench\PHPUnit;
 use function Orchestra\Testbench\phpunit_version_compare;
 use Throwable;
 
-if (phpunit_version_compare('10.0.0', '<')) {
+if (phpunit_version_compare('10.1.0', '<')) {
+    class TestCase extends \PHPUnit\Framework\TestCase
+    {
+        /**
+         * {@inheritdoc}
+         */
+        protected function onNotSuccessfulTest(Throwable $exception): never
+        {
+            /** @var \Illuminate\Testing\TestResponse|null $response */
+            $response = static::$latestResponse ?? null;
+
+            parent::onNotSuccessfulTest(
+                \is_null($response)
+                    ? $response->transformNotSuccessfulException($exception)
+                    : $exception
+            );
+        }
+    }
+} elseif (phpunit_version_compare('10.0.0', '<')) {
     class TestCase extends \PHPUnit\Framework\TestCase
     {
         /**
