a0bfbf111c88948307f3945829a87708d52e35bb
Mior Muhammad Zaki <crynobone@gmail.com>
2023-06-07 07:31:20 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Bootstrap/LoadEnvironmentVariables.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Orchestra\Testbench\Bootstrap;
+
+final class LoadEnvironmentVariables extends \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables
+{
+    /**
+     * Create a Dotenv instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return \Dotenv\Dotenv
+     */
+    protected function createDotenv($app)
+    {
+        if (! file_exists(implode(DIRECTORY_SEPARATOR, [$app->environmentPath(), $app->environmentFile()]))) {
+            $this->setEnvironmentFilePath($app, '.env.testbench');
+        }
+
+        return parent::createDotenv($app);
+    }
+}
