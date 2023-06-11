dd358ce0fa71fbd4e8fdd076b89a143d577d2e9e
Mior Muhammad Zaki <crynobone@gmail.com>
2023-06-11 20:55:39 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Foundation/PackageManifestTest.php
+++ tests/Foundation/PackageManifestTest.php
@@ -31,8 +31,6 @@ class PackageManifestTest extends TestCase
 
         $installedPackages = [
             'nesbot/carbon',
-            'orchestra/canvas',
-            'orchestra/canvas-core',
         ];
 
         foreach ($installedPackages as $installedPackage) {
