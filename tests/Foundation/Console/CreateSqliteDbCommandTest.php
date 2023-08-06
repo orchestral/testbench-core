a5232cb5a93912660e5cca4ec48cd48084d33356
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-06 20:51:15 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Foundation/Console/CreateSqliteDbCommandTest.php
+++ tests/Foundation/Console/CreateSqliteDbCommandTest.php
@@ -2,10 +2,7 @@
 
 namespace Orchestra\Testbench\Tests\Foundation\Console;
 
-use Orchestra\Testbench\Foundation\TestbenchServiceProvider;
-use Orchestra\Testbench\TestCase;
-use Orchestra\Testbench\Tests\Concerns\Database\InteractsWithSqliteDatabase;
-use Orchestra\Testbench\Workbench\WorkbenchServiceProvider;
+use Orchestra\Testbench\Tests\Workbench\Console\CreateSqliteDbCommandTest as TestCase;
 
 /**
  * @requires OS Linux|DAR
@@ -14,45 +11,5 @@ use Orchestra\Testbench\Workbench\WorkbenchServiceProvider;
  */
 class CreateSqliteDbCommandTest extends TestCase
 {
-    use InteractsWithSqliteDatabase;
-
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            TestbenchServiceProvider::class,
-            WorkbenchServiceProvider::class,
-        ];
-    }
-
-    /** @test */
-    public function it_can_generate_database_using_command()
-    {
-        $this->withoutSqliteDatabase(function () {
-            $this->assertFalse(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
-
-            $this->artisan('package:create-sqlite-db')
-                ->expectsOutputToContain('Copying file [database/database.sqlite.example] to [database/database.sqlite]')
-                ->assertOk();
-
-            $this->assertTrue(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
-        });
-    }
-
-    /** @test */
-    public function it_cannot_generate_database_using_command_when_database_already_exists()
-    {
-        $this->withSqliteDatabase(function () {
-            $this->assertTrue(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
-
-            $this->artisan('package:create-sqlite-db')
-                ->expectsOutputToContain('File [database/database.sqlite] already exists')
-                ->assertOk();
-        });
-    }
+    protected $namespace = 'package';
 }
--- tests/Foundation/Console/DropSqliteDbCommandTest.php
+++ tests/Foundation/Console/DropSqliteDbCommandTest.php
@@ -2,10 +2,7 @@
 
 namespace Orchestra\Testbench\Tests\Foundation\Console;
 
-use Orchestra\Testbench\Foundation\TestbenchServiceProvider;
-use Orchestra\Testbench\TestCase;
-use Orchestra\Testbench\Tests\Concerns\Database\InteractsWithSqliteDatabase;
-use Orchestra\Testbench\Workbench\WorkbenchServiceProvider;
+use Orchestra\Testbench\Tests\Workbench\Console\DropSqliteDbCommandTest as TestCase;
 
 /**
  * @requires OS Linux|DAR
@@ -14,45 +11,5 @@ use Orchestra\Testbench\Workbench\WorkbenchServiceProvider;
  */
 class DropSqliteDbCommandTest extends TestCase
 {
-    use InteractsWithSqliteDatabase;
-
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            TestbenchServiceProvider::class,
-            WorkbenchServiceProvider::class,
-        ];
-    }
-
-    /** @test */
-    public function it_can_drop_database_using_command()
-    {
-        $this->withSqliteDatabase(function () {
-            $this->assertTrue(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
-
-            $this->artisan('package:drop-sqlite-db')
-                ->expectsOutputToContain('File [database/database.sqlite] has been deleted')
-                ->assertOk();
-
-            $this->assertFalse(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
-        });
-    }
-
-    /** @test */
-    public function it_cannot_drop_database_using_command_when_database_doesnt_exists()
-    {
-        $this->withoutSqliteDatabase(function () {
-            $this->assertFalse(file_exists(__DIR__.'/../../../laravel/database/database.sqlite'));
-
-            $this->artisan('package:drop-sqlite-db')
-                ->expectsOutputToContain('File [database/database.sqlite] does not exists')
-                ->assertOk();
-        });
-    }
+    protected $namespace = 'workbench';
 }
