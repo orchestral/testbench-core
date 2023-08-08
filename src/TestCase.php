932456c5de51e7c57662547b488915057b094dee
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-08 12:16:02 +0800

    Integrate Workbench to TestCase (#122)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -42,6 +42,13 @@ abstract class TestCase extends PHPUnit implements Contracts\TestCase
     protected $enablesPackageDiscoveries = false;
 
     /**
+     * The cached uses for test case.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected static $cachedTestCaseUses = [];
+
+    /**
      * Setup the test environment.
      *
      * @return void
@@ -123,6 +130,21 @@ abstract class TestCase extends PHPUnit implements Contracts\TestCase
     }
 
     /**
+     * Prepare the testing environment before the running the test case.
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass(): void
+    {
+        /** @var array<class-string, class-string> $uses */
+        $uses = array_flip(class_uses_recursive(static::class));
+
+        static::setupBeforeClassUsingWorkbench();
+
+        static::$cachedTestCaseUses = $uses;
+    }
+
+    /**
      * Clean up the testing environment before the next test case.
      *
      * @return void
@@ -130,6 +152,10 @@ abstract class TestCase extends PHPUnit implements Contracts\TestCase
     public static function tearDownAfterClass(): void
     {
         static::$latestResponse = null;
+
+        static::teardownAfterClassUsingWorkbench();
+
+        static::$cachedTestCaseUses = [];
     }
 
     /**
--- tests/Databases/RefreshDatabaseTest.php
+++ tests/Databases/RefreshDatabaseTest.php
@@ -4,11 +4,12 @@ namespace Orchestra\Testbench\Tests\Databases;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Orchestra\Testbench\artisan;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase;
 
 class RefreshDatabaseTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithWorkbench;
 
     /**
      * Define environment setup.
@@ -31,25 +32,12 @@ class RefreshDatabaseTest extends TestCase
         artisan($this, 'migrate', ['--database' => 'testing']);
     }
 
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            \Orchestra\Testbench\Tests\Fixtures\Providers\ServiceProvider::class,
-        ];
-    }
-
     /** @test */
     public function it_runs_the_migrations()
     {
         $users = \DB::table('testbench_users')->where('id', '=', 1)->first();
 
-        $this->assertEquals('hello@orchestraplatform.com', $users->email);
+        $this->assertEquals('crynobone@gmail.com', $users->email);
         $this->assertTrue(\Hash::check('123', $users->password));
 
         $this->assertEquals([
