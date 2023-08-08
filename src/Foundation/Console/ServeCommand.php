43502e519818a62140a9ce4cff2f764e1643cfd5
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-08 14:51:16 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/ServeCommand.php
+++ src/Foundation/Console/ServeCommand.php
@@ -3,9 +3,25 @@
 namespace Orchestra\Testbench\Foundation\Console;
 
 use Illuminate\Foundation\Console\ServeCommand as Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ServeCommand extends Command
 {
+    /**
+     * Initializes the command after the input has been bound and before the input
+     * is validated.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $_ENV['TESTBENCH_WORKING_PATH'] = TESTBENCH_WORKING_PATH;
+        static::$passthroughVariables[] = 'TESTBENCH_WORKING_PATH';
+    }
+
     /**
      * Get the value of a command option.
      *
@@ -17,7 +33,7 @@ class ServeCommand extends Command
         $value = parent::option($key);
 
         if ($key === 'no-reload' && $value !== true) {
-            $value = true;
+            return true;
         }
 
         return $value;
