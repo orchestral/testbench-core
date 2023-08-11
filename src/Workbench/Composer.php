7aecb529e4f1475eade12bf8058506be3735a44e
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-11 09:12:04 +0800

    Throw JSON exception when failing to decode `composer.json`
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Workbench/Composer.php
+++ src/Workbench/Composer.php
@@ -14,7 +14,7 @@ class Composer extends \Illuminate\Support\Composer
     {
         $composerFile = "{$this->workingPath}/composer.json";
 
-        $composer = json_decode((string) file_get_contents($composerFile), true);
+        $composer = json_decode((string) file_get_contents($composerFile), true, 512, JSON_THROW_ON_ERROR);
 
         $composer = \call_user_func($callback, $composer);
 
