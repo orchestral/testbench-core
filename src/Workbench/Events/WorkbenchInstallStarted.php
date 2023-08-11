806c19f162705f200ad5c115ca5e765f4588a405
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-12 07:41:12 +0800

    Workbench Events (#128)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Workbench/Events/WorkbenchInstallStarted.php
+++ src/Workbench/Events/WorkbenchInstallStarted.php
@@ -6,7 +6,7 @@ use Illuminate\Console\View\Components\Factory;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ServeCommandEnded
+class WorkbenchInstallStarted
 {
     /**
      * Construct a new event.
@@ -14,13 +14,11 @@ class ServeCommandEnded
      * @param  \Symfony\Component\Console\Input\InputInterface  $input
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
      * @param  \Illuminate\Console\View\Components\Factory  $components
-     * @param  int  $exitCode
      */
     public function __construct(
         public InputInterface $input,
         public OutputInterface $output,
-        public Factory $components,
-        public int $exitCode
+        public Factory $components
     ) {
         //
     }
