53e301da88c7bcae8354f3a0d45362f645cca193
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-13 19:12:32 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Foundation/Console/ClearSkeletonCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Orchestra\Testbench\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'package:clear-skeleton', description: 'Clear skeleton folder to original state')]
+class ClearSkeletonCommand extends Command
+{
+    use Concerns\InteractsWithIO;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'package:clear-skeleton';
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $filesystem
+     * @return int
+     */
+    public function handle(Filesystem $filesystem)
+    {
+        $workingPath = $this->laravel->basePath();
+
+        Collection::make([
+            '.env',
+            'testbench.yaml',
+            'database/database.sqlite',
+        ])->map(fn ($file) => $this->laravel->basePath($file))
+        ->filter(fn ($file) => $filesystem->exists($file))
+        ->each(function ($file) use ($filesystem, $workingPath) {
+            $filesystem->delete($file);
+
+            $this->components->twoColumnDetail(
+                sprintf('File [%s] has been deleted', str_replace($workingPath.'/', '', $file)),
+                '<fg=green;options=bold>DONE</>'
+            );
+        });
+
+        $filesystem->delete(
+            $this->laravel->bootstrapPath('cache/routes-v7.php'),
+            ...$filesystem->glob($this->laravel->basePath('routes/testbench-*.php'))
+        );
+
+        return Command::SUCCESS;
+    }
+}
