20fb592fbd49a92a84dffc3ded520e13f6fdd88b
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-17 08:34:10 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/TestbenchServiceProvider.php
+++ src/Foundation/TestbenchServiceProvider.php
@@ -39,7 +39,6 @@ class TestbenchServiceProvider extends ServiceProvider
                     ? Console\TestCommand::class
                     : Console\TestFallbackCommand::class,
                 Console\CreateSqliteDbCommand::class,
-                Console\DevToolCommand::class,
                 Console\DropSqliteDbCommand::class,
                 Console\PurgeSkeletonCommand::class,
                 Console\ServeCommand::class,
