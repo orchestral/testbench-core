4e1876a2771139570c47232fa95e672de960456a
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-28 11:32:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Foundation/Console/Actions/EnsureDirectoryExistsTest.php
+++ tests/Foundation/Console/Actions/EnsureDirectoryExistsTest.php
@@ -17,17 +17,17 @@ class EnsureDirectoryExistsTest extends TestCase
         $components = m::mock(ComponentsFactory::class);
 
         $filesystem->shouldReceive('isDirectory')->once()->with('a')->andReturnFalse()
-            ->shouldReceive('isDirectory')->once()->with('b')->andReturnFalse()
-            ->shouldReceive('isDirectory')->once()->with('c/d')->andReturnFalse()
             ->shouldReceive('ensureDirectoryExists')->once()->with('a', 493, true)->andReturnSelf()
             ->shouldReceive('copy')->once()->with(M::type('String'), 'a/.gitkeep')->andReturnSelf()
-            ->shouldReceive('ensureDirectoryExists')->once()->with('b', 493, true)->andReturnSelf()
-            ->shouldReceive('copy')->once()->with(M::type('String'), 'b/.gitkeep')->andReturnSelf()
+            ->shouldReceive('isDirectory')->once()->with('b')->andReturnTrue()
+            ->shouldReceive('ensureDirectoryExists')->never()->with('b', 493, true)->andReturnSelf()
+            ->shouldReceive('copy')->never()->with(M::type('String'), 'b/.gitkeep')->andReturnSelf()
+            ->shouldReceive('isDirectory')->once()->with('c/d')->andReturnFalse()
             ->shouldReceive('ensureDirectoryExists')->once()->with('c/d', 493, true)->andReturnSelf()
             ->shouldReceive('copy')->once()->with(M::type('String'), 'c/d/.gitkeep')->andReturnSelf();
 
         $components->shouldReceive('task')->once()->with('Prepare [a] directory')->andReturnNull()
-            ->shouldReceive('task')->once()->with('Prepare [b] directory')->andReturnNull()
+            ->shouldReceive('twoColumnDetail')->once()->with('Directory [b] already exists', '<fg=yellow;options=bold>SKIPPED</>')->andReturnNull()
             ->shouldReceive('task')->once()->with('Prepare [c/d] directory')->andReturnNull();
 
         (new EnsureDirectoryExists(
