5b287bf2f438cd3bd589480190734abb149aafa6
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-28 17:37:35 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- sync.sh
+++ sync.sh
@@ -33,6 +33,7 @@ awk '{sub(/production/,"testing")}1' laravel/config/app.php > laravel/config/app
 awk '{sub(/App\\Providers/,"// App\\Providers")}1' laravel/config/app.php > laravel/config/app.stub && mv laravel/config/app.stub laravel/config/app.php
 awk '{sub(/\x27AUTH_MODEL\x27, App\\Models\\User/,"'\''AUTH_MODEL'\'', Illuminate\\Foundation\\Auth\\User")}1' laravel/config/auth.php > laravel/config/auth.stub && mv laravel/config/auth.stub laravel/config/auth.php
 awk '{sub(/\x27CACHE_STORE\x27, \x27file\x27/,"'\''CACHE_STORE'\'', '\''array'\''")}1' laravel/config/cache.php > laravel/config/cache.stub && mv laravel/config/cache.stub laravel/config/cache.php
+awk '{sub(/\x27QUEUE_CONNECTION\x27, \x27database\x27/,"'\''QUEUE_CONNECTION'\'', '\''sync'\''")}1' laravel/config/queue.php > laravel/config/queue.stub && mv laravel/config/queue.stub laravel/config/queue.php
 awk '{sub(/\x27SESSION_DRIVER\x27, \x27file\x27/,"'\''SESSION_DRIVER'\'', '\''array'\''")}1' laravel/config/session.php > laravel/config/session.stub && mv laravel/config/session.stub laravel/config/session.php
 awk '{sub(/use App\\Models\\User/,"use Illuminate\\Foundation\\Auth\\User")}1' src/Factories/UserFactory.php > src/Factories/UserFactory.stub && mv src/Factories/UserFactory.stub src/Factories/UserFactory.php
 awk '{sub(/namespace Database\\Factories/,"namespace Orchestra\\Testbench\\Factories")}1' src/Factories/UserFactory.php > src/Factories/UserFactory.stub && mv src/Factories/UserFactory.stub src/Factories/UserFactory.php
