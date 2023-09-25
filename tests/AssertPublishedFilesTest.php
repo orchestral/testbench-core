756eed87e36936607aeb183dca87c608cd376930
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-25 21:32:56 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/AssertPublishedFilesTest.php
+++ tests/AssertPublishedFilesTest.php
@@ -42,12 +42,11 @@ class AssertPublishedFilesTest extends TestCase
             'Schema::create(\'users\', function (Blueprint $table) {',
         ], 'testbench_create_users_table.php', directory: 'migrations');
 
-
         $this->assertMigrationFileDoesNotContains([
             'class TestbenchCreateUsersTable extends Migration',
         ], 'testbench_create_users_table.php', directory: 'migrations');
 
-        $this->assertMigrationFileExists('2014_10_12_100000_testbench_create_password_resets_table.php', 'migrations');
-        $this->assertMigrationFileDoesNotExists('2014_10_12_100000_create_password_resets_table.php', 'migrations');
+        $this->assertMigrationFileExists('2014_10_12_100000_testbench_create_password_reset_tokens_table.php', 'migrations');
+        $this->assertMigrationFileDoesNotExists('2014_10_12_100000_create_password_resets_tokens_table.php', 'migrations');
     }
 }
