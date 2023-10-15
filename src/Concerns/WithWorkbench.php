c71590c9affcfc1a17bbed6dd235beaa102c8752
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-15 15:36:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/WithWorkbench.php
+++ src/Concerns/WithWorkbench.php
@@ -4,6 +4,7 @@ namespace Orchestra\Testbench\Concerns;
 
 use Orchestra\Testbench\Foundation\Bootstrap\LoadMigrationsFromArray;
 use Orchestra\Testbench\Foundation\Bootstrap\StartWorkbench;
+use Orchestra\Workbench\Workbench;
 
 trait WithWorkbench
 {
@@ -27,5 +28,7 @@ trait WithWorkbench
         (new LoadMigrationsFromArray(
             $config['migrations'] ?? [], $config['seeders'] ?? false,
         ))->bootstrap($app);
+
+        Workbench::discover($app);
     }
 }
---
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Workbench\Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
