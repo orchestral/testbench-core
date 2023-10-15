c3fe1b4bbf76f8a22fd69468441b1e6b558c4d19
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-15 17:48:25 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/routes/api.php
+++ workbench/routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return response()->json('hello world');
 });
