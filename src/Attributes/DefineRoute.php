7f24bc8fdf109b1d22135f9267a97c04567a2f8a
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-23 15:02:16 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/DefineRoute.php
+++ src/Attributes/DefineRoute.php
@@ -7,9 +7,20 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class DefineRoute
 {
-    public function __construct(
-        public string $method
-    ) {
-        //
+    /**
+     * The target method.
+     *
+     * @var string
+     */
+    public $method;
+
+    /**
+     * Construct a new attribute.
+     *
+     * @param  string  $method
+     */
+    public function __construct(string $method)
+    {
+        $this->method = $method;
     }
 }
