d33495bac05970ef155a54b3e341ad2adbffa280
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-23 15:59:07 +0800

    PHPUnit Attribute Support (#144)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Attributes/DefineDatabase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Orchestra\Testbench\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+final class DefineDatabase
+{
+    /**
+     * The target method.
+     *
+     * @var string
+     */
+    public $method;
+
+    /**
+     * Construct a new attribute.
+     *
+     * @param  string  $method
+     */
+    public function __construct(string $method)
+    {
+        $this->method = $method;
+    }
+}
--- src/Concerns/CreatesApplication.php
+++ src/Concerns/CreatesApplication.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\RateLimiter;
+use Orchestra\Testbench\Attributes\DefineEnvironment;
 use Orchestra\Testbench\Bootstrap\LoadEnvironmentVariables;
 use Orchestra\Testbench\Foundation\PackageManifest;
 
@@ -364,9 +365,16 @@ trait CreatesApplication
             $app->register('Illuminate\Database\Eloquent\LegacyFactoryServiceProvider');
         }
 
-        if (method_exists($this, 'parseTestMethodAnnotations')) {
+        if (static::usesTestingConcern(HandlesAnnotations::class)) {
+            /** @phpstan-ignore-next-line */
             $this->parseTestMethodAnnotations($app, 'environment-setup');
+            /** @phpstan-ignore-next-line */
             $this->parseTestMethodAnnotations($app, 'define-env');
+        }
+
+        if (static::usesTestingConcern(HandlesAttributes::class)) {
+            /** @phpstan-ignore-next-line */
+            $this->parseTestMethodAttributes($app, DefineEnvironment::class);
         }
 
         $this->defineEnvironment($app);
