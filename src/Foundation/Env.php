074685da6b14e400ed9f88f13ae410090ed4307f
Mior Muhammad Zaki <crynobone@gmail.com>
2023-11-08 22:04:22 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Env.php
+++ src/Foundation/Env.php
@@ -14,12 +14,8 @@ class Env extends \Illuminate\Support\Env
      * @param  \Orchestra\Testbench\Foundation\UndefinedValue|mixed|null  $default
      * @return mixed
      */
-    public static function forward(string $key, $default = null)
+    public static function forward(string $key, $default = new UndefinedValue())
     {
-        if (\func_num_args() === 1) {
-            $default = new UndefinedValue();
-        }
-
         $value = static::get($key, $default);
 
         if ($value instanceof UndefinedValue) {
