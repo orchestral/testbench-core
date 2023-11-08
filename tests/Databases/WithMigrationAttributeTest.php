41e0f879e2f44b756c802c31c0f13ad35a448264
Mior Muhammad Zaki <crynobone@gmail.com>
2023-11-08 20:18:56 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Databases/WithMigrationAttributeTest.php
+++ tests/Databases/WithMigrationAttributeTest.php
@@ -27,8 +27,75 @@ class WithMigrationAttributeTest extends TestCase
     }
 
     /** @test */
-    public function it_loads_the_laravel_migrations()
+    public function it_loads_default_migrations()
     {
         $this->assertTrue(Schema::hasTable('users'));
+        $this->assertTrue(Schema::hasTable('password_resets'));
+        $this->assertFalse(Schema::hasTable('cache'));
+        $this->assertFalse(Schema::hasTable('cache_locks'));
+        $this->assertFalse(Schema::hasTable('jobs'));
+        $this->assertFalse(Schema::hasTable('job_batches'));
+        $this->assertTrue(Schema::hasTable('failed_jobs'));
+        $this->assertFalse(Schema::hasTable('notifications'));
+        $this->assertFalse(Schema::hasTable('sessions'));
+    }
+
+    /** @test */
+    #[WithMigration('cache')]
+    public function it_loads_caches_migrations()
+    {
+        $this->assertTrue(Schema::hasTable('users'));
+        $this->assertTrue(Schema::hasTable('password_resets'));
+        $this->assertTrue(Schema::hasTable('cache'));
+        $this->assertTrue(Schema::hasTable('cache_locks'));
+        $this->assertFalse(Schema::hasTable('jobs'));
+        $this->assertFalse(Schema::hasTable('job_batches'));
+        $this->assertTrue(Schema::hasTable('failed_jobs'));
+        $this->assertFalse(Schema::hasTable('notifications'));
+        $this->assertFalse(Schema::hasTable('sessions'));
+    }
+
+    /** @test */
+    #[WithMigration('notifications')]
+    public function it_loads_notifications_migrations()
+    {
+        $this->assertTrue(Schema::hasTable('users'));
+        $this->assertTrue(Schema::hasTable('password_resets'));
+        $this->assertFalse(Schema::hasTable('cache'));
+        $this->assertFalse(Schema::hasTable('cache_locks'));
+        $this->assertFalse(Schema::hasTable('jobs'));
+        $this->assertFalse(Schema::hasTable('job_batches'));
+        $this->assertTrue(Schema::hasTable('failed_jobs'));
+        $this->assertTrue(Schema::hasTable('notifications'));
+        $this->assertFalse(Schema::hasTable('sessions'));
+    }
+
+    /** @test */
+    #[WithMigration('queue')]
+    public function it_loads_queue_migrations()
+    {
+        $this->assertTrue(Schema::hasTable('users'));
+        $this->assertTrue(Schema::hasTable('password_resets'));
+        $this->assertFalse(Schema::hasTable('cache'));
+        $this->assertFalse(Schema::hasTable('cache_locks'));
+        $this->assertTrue(Schema::hasTable('jobs'));
+        $this->assertTrue(Schema::hasTable('job_batches'));
+        $this->assertTrue(Schema::hasTable('failed_jobs'));
+        $this->assertFalse(Schema::hasTable('notifications'));
+        $this->assertFalse(Schema::hasTable('sessions'));
+    }
+    /** @test */
+    #[WithMigration('session')]
+    public function it_loads_session_migrations()
+    {
+        $this->assertTrue(Schema::hasTable('users'));
+        $this->assertTrue(Schema::hasTable('password_resets'));
+        $this->assertFalse(Schema::hasTable('cache'));
+        $this->assertFalse(Schema::hasTable('cache_locks'));
+        $this->assertFalse(Schema::hasTable('jobs'));
+        $this->assertFalse(Schema::hasTable('job_batches'));
+        $this->assertTrue(Schema::hasTable('failed_jobs'));
+        $this->assertFalse(Schema::hasTable('notifications'));
+        $this->assertTrue(Schema::hasTable('sessions'));
     }
 }
