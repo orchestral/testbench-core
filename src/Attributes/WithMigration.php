c3330b41386deae7818457f1aad1701471f0014e
Mior Muhammad Zaki <crynobone@gmail.com>
2023-11-09 08:37:33 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/WithMigration.php
+++ src/Attributes/WithMigration.php
@@ -19,6 +19,6 @@ final class WithMigration
      */
     public function __construct()
     {
-        $this->types = array_merge(['laravel'], \func_get_args());
+        $this->types = \func_num_args() > 0 ? \func_get_args() : ['laravel'];
     }
 }
