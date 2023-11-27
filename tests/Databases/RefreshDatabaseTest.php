a35b26bb1f29afe16ac4eb68467c6fa233328ab0
Mior Muhammad Zaki <crynobone@gmail.com>
2023-11-27 22:16:21 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Databases/RefreshDatabaseTest.php
+++ tests/Databases/RefreshDatabaseTest.php
@@ -71,9 +71,11 @@ class RefreshDatabaseTest extends TestCase
 
     public function addAdditionalTableAtRuntime()
     {
-        Schema::create('testbench_auths', function (Blueprint $table) {
-            $table->id();
-            $table->text('two_factor_secret')->nullable();
+        $this->afterApplicationCreated(function () {
+            Schema::create('testbench_auths', function (Blueprint $table) {
+                $table->id();
+                $table->text('two_factor_secret')->nullable();
+            });
         });
     }
 }
