1b35ab010c5d0dd6b9b15449ed1cdccf62316ddb
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-06 14:14:24 +0800

    Testbench CLI should handle SIGTERM and SIGQUIT signal
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commander.php
+++ src/Console/Commander.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Testbench\Console;
 
+use Illuminate\Console\Command;
 use Illuminate\Console\Concerns\InteractsWithSignals;
 use Illuminate\Console\Signals;
 use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
@@ -22,7 +23,6 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\SignalRegistry\SignalRegistry;
 use Throwable;
-
 use function Orchestra\Testbench\transform_relative_path;
 
 class Commander
@@ -94,6 +94,8 @@ class Commander
         } finally {
             $this->handleTerminatingConsole();
             Workbench::flush();
+
+            $this->untrap();
         }
 
         exit($status);
@@ -221,9 +223,22 @@ class Commander
         Signals::whenAvailable(function () {
             $this->signals ??= new Signals(new SignalRegistry());
 
-            Collection::make(Arr::wrap([SIGINT]))
+            Collection::make(Arr::wrap([SIGINT, SIGTERM, SIGQUIT]))
                 ->each(
-                    fn ($signal) => $this->signals->register($signal, fn () => $this->handleTerminatingConsole())
+                    fn ($signal) => $this->signals->register($signal, function () use ($signal) {
+                        $this->handleTerminatingConsole();
+                        Workbench::flush();
+
+                        $status = match ($signal) {
+                            SIGINT => 130,
+                            SIGTERM => 143,
+                            default => 128 + $signal,
+                        };
+
+                        $this->untrap();
+
+                        exit($status);
+                    })
                 );
         });
     }
