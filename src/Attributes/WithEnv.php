76e580f0aa0c5a16d021a8cdb8728fd9c00c3cd6
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-13 06:13:12 +0800

    Add new `WithEnv` attribute (#167)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Attributes/WithEnv.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Orchestra\Testbench\Attributes;
+
+use Attribute;
+use Orchestra\Testbench\Contracts\Attributes\Invokable as InvokableContract;
+use Orchestra\Testbench\Foundation\Env;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+final class WithEnv implements InvokableContract
+{
+    /**
+     * Construct a new attribute.
+     */
+    public function __construct(
+        public string $key,
+        public string $value
+    ) {
+        //
+    }
+
+    /**
+     * Handle the attribute.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     */
+    public function __invoke($app): void
+    {
+        Env::set($this->key, $this->value);
+    }
+}
