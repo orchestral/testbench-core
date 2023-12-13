d18c595b42c8c9e6d6b091989ff4c438c976653d
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-13 11:28:30 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Attributes/WithConfigTest.php
+++ tests/Attributes/WithConfigTest.php
@@ -4,20 +4,18 @@ namespace Orchestra\Testbench\Tests\Attributes;
 
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
-/**
- * @requires PHP >= 8.0
- */
 class WithConfigTest extends TestCase
 {
-    /** @test */
+    #[Test]
     #[WithConfig('testbench.attribute', true)]
     public function it_can_resolve_defined_configuration()
     {
         $this->assertSame(true, config('testbench.attribute'));
     }
 
-    /** @test */
+    #[Test]
     public function it_does_not_persist_defined_configuration_between_tests()
     {
         $this->assertNull(config('testbench.attribute'));
--- tests/Attributes/WithEnvTest.php
+++ tests/Attributes/WithEnvTest.php
@@ -5,20 +5,17 @@ namespace Orchestra\Testbench\Tests\Attributes;
 use Orchestra\Testbench\Attributes\WithEnv;
 use Orchestra\Testbench\Foundation\Env;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
-/**
- * @requires PHP >= 8.0
- */
 class WithEnvTest extends TestCase
 {
-    /** @test */
+    #[Test]
     #[WithEnv('TESTING_USING_ATTRIBUTE', '(true)')]
     public function it_can_resolve_defined_env_variables()
     {
         $this->assertSame(true, Env::get('TESTING_USING_ATTRIBUTE'));
     }
-
-    /** @test */
+    #[Test]
     public function it_does_not_persist_defined_env_variables_between_tests()
     {
         $this->assertNull(Env::get('TESTING_USING_ATTRIBUTE'));
