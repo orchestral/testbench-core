c94f48cb1dda9b18d444b8d300bdc2719296d38f
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-13 16:37:53 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integrations/CacheRouteTest.php
+++ tests/Integrations/CacheRouteTest.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Testbench\Tests\Integrations;
 
+use Illuminate\Support\Facades\Log;
 use Orchestra\Testbench\TestCase;
 
 class CacheRouteTest extends TestCase
@@ -14,7 +15,13 @@ class CacheRouteTest extends TestCase
         $this->defineCacheRoutes(<<<PHP
 <?php
 
+use Psr\Log\LoggerInterface;
+
 Route::get('stubs-controller', 'Workbench\App\Http\Controllers\ExampleController@index');
+
+Route::any('/logger', function (LoggerInterface \$log) {
+    \$log->info('hello');
+})->where(['all' => '.*']);
 PHP);
 
         parent::setUp();
@@ -30,5 +37,18 @@ PHP);
         $this->get('stubs-controller')
             ->assertOk()
             ->assertSee('ExampleController@index');
+    }
+
+    /**
+     * @test
+     *
+     * @group without-parallel
+     */
+    public function it_can_cache_closure_route()
+    {
+        Log::spy()->shouldReceive('info')->with('hello');
+
+        $this->get('logger')
+            ->assertOk();
     }
 }
