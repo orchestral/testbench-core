c27a8130936b2e054bd44ca687204e9c84127c90
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-15 08:26:49 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Database/ConnectionFactory.php
+++ src/Database/ConnectionFactory.php
@@ -5,6 +5,13 @@ namespace Orchestra\Testbench\Database;
 class ConnectionFactory extends \Illuminate\Database\Connectors\ConnectionFactory
 {
     /**
+     * List of cached database connections.
+     *
+     * @var array<string, \Illuminate\Database\Connection>
+     */
+    protected static array $cachedConnections = [];
+
+    /**
      * Establish a PDO connection based on the configuration.
      *
      * @param  array  $config
@@ -14,12 +21,8 @@ class ConnectionFactory extends \Illuminate\Database\Connectors\ConnectionFactor
     #[\Override]
     public function make(array $config, $name = null)
     {
-        $config = $this->parseConfig($config, $name);
+        $connection = $name ?? $config['name'];
 
-        if (isset($config['read'])) {
-            return $this->createReadWriteConnection($config);
-        }
-
-        return $this->createSingleConnection($config);
+        return static::$cachedConnections[$name] ??= parent::make($config, $name);
     }
 }
