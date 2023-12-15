9092487ba84e43f5d471d116dc56ed2f7eda34ce
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-15 10:10:13 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Database/ConnectionFactory.php
+++ src/Database/ConnectionFactory.php
@@ -24,24 +24,24 @@ class ConnectionFactory extends \Illuminate\Database\Connectors\ConnectionFactor
     #[\Override]
     public function make(array $config, $name = null)
     {
-        /** @var string $name */
-        $name = $name ?? $config['name'];
+        /** @var string $key */
+        $key = $name ?? $config['name'];
 
         if ($config['driver'] === 'sqlite') {
             return parent::make($config, $name);
         }
 
-        if (! isset(static::$cachedConnections[$name]) || \is_null((static::$cachedConnections[$name]->getRawPdo() ?? null))) {
-            return static::$cachedConnections[$name] = parent::make($config, $name);
+        if (! isset(static::$cachedConnections[$key]) || \is_null((static::$cachedConnections[$key]->getRawPdo() ?? null))) {
+            return static::$cachedConnections[$key] = parent::make($config, $name);
         }
 
         $config = $this->parseConfig($config, $name);
 
         $connection = $this->createConnection(
-            $config['driver'], static::$cachedConnections[$name]->getRawPdo(), $config['database'], $config['prefix'], $config
-        )->setReadPdo(static::$cachedConnections[$name]->getRawReadPdo());
+            $config['driver'], static::$cachedConnections[$key]->getRawPdo(), $config['database'], $config['prefix'], $config
+        )->setReadPdo(static::$cachedConnections[$key]->getRawReadPdo());
 
-        return static::$cachedConnections[$name] = $connection;
+        return static::$cachedConnections[$key] = $connection;
     }
 
     /**
