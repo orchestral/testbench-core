3205c71ac63bddb6a54e7474c065a4bfa3ad4be0
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-26 09:26:53 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/ResetRefreshDatabaseState.php
+++ src/Attributes/ResetRefreshDatabaseState.php
@@ -15,14 +15,23 @@ final class ResetRefreshDatabaseState implements AfterAllContract, BeforeAllCont
      */
     public function beforeAll(): void
     {
-        RefreshDatabaseState::$migrated = false;
-        RefreshDatabaseState::$lazilyRefreshed = false;
+        self::run();
     }
 
     /**
      * Handle the attribute.
      */
     public function afterAll(): void
+    {
+        self::run();
+    }
+
+    /**
+     * Execute the action.
+     *
+     * @return void
+     */
+    public static function run(): void
     {
         RefreshDatabaseState::$migrated = false;
         RefreshDatabaseState::$lazilyRefreshed = false;
