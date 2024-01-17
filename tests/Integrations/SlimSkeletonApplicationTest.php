a80874fc80f91ac5c8a955040d6509e83285b5ff
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-17 08:11:09 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integrations/SlimSkeletonApplicationTest.php
+++ tests/Integrations/SlimSkeletonApplicationTest.php
@@ -2,8 +2,10 @@
 
 namespace Orchestra\Testbench\Tests\Integrations;
 
+use Orchestra\Testbench\Attributes\DefineRoute;
 use Orchestra\Testbench\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use function Illuminate\Filesystem\join_paths;
 use function Orchestra\Testbench\workbench_path;
 
@@ -26,8 +28,36 @@ class SlimSkeletonApplicationTest extends TestCase
     #[Test]
     public function it_can_access_welcome_page_using_route_name()
     {
-        $response = $this->get(route('welcome'));
+        $this->get(route('welcome'))
+            ->assertOk();
+    }
 
-        $response->assertOk();
+    #[Test]
+    public function it_throws_exception_when_trying_to_access_authenticated_routes_as_guest_without_login_route_name()
+    {
+        $this->expectException(RouteNotFoundException::class);
+        $this->expectExceptionMessage('Route [login] not defined.');
+
+        $this->withoutExceptionHandling()
+            ->get(route('dashboard'));
+    }
+
+    #[Test]
+    #[DefineRoute('defineLoginRoutes')]
+    public function it_can_be_redirected_to_login_route_name_when_trying_to_access_authenticated_routes()
+    {
+        $this->get(route('dashboard'))
+            ->assertRedirectToRoute('login');
+    }
+
+    /**
+     * Define login routes setup.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function defineLoginRoutes($router)
+    {
+        $router->get('/login', fn () => 'Login')->name('login');
     }
 }
