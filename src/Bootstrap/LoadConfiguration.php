a98dc80d47263f33610b5d71d5bcf462a45440dd
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-18 22:07:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Bootstrap/LoadConfiguration.php
+++ src/Bootstrap/LoadConfiguration.php
@@ -10,6 +10,8 @@ use Illuminate\Support\LazyCollection;
 use Orchestra\Testbench\Foundation\Env;
 use Symfony\Component\Finder\Finder;
 
+use function Orchestra\Testbench\default_skeleton_path;
+
 /**
  * @internal
  */
@@ -51,7 +53,7 @@ class LoadConfiguration
             LazyCollection::make(static function () use ($app) {
                 $path = is_dir($app->basePath('config'))
                     ? $app->basePath('config')
-                    : realpath(__DIR__.'/../../laravel/config');
+                    : default_skeleton_path('config');
 
                 if (\is_string($path)) {
                     foreach (Finder::create()->files()->name('*.php')->in($path) as $file) {
