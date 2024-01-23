fe40cd88d46141af754ad355e8b142a1bc2428d2
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-23 23:11:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Databases/MigrateWithLaravelMigrationsUsingRefreshDatabaseTest.php
+++ tests/Databases/MigrateWithLaravelMigrationsUsingRefreshDatabaseTest.php
@@ -6,24 +6,15 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\Concerns\WithLaravelMigrations;
 use Orchestra\Testbench\TestCase;
 
+#[WithConfig('database.default', 'testing')]
 class MigrateWithLaravelMigrationsUsingRefreshDatabaseTest extends TestCase
 {
     use RefreshDatabase;
     use WithLaravelMigrations;
-
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function defineEnvironment($app)
-    {
-        $app['config']->set('database.default', 'testing');
-    }
 
     /** @test */
     public function it_loads_the_migrations()
