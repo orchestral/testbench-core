7dacc05a1ae1a42039a0a142d10d4270b9ef8efa
Mior Muhammad Zaki <crynobone@gmail.com>
2024-02-13 21:45:15 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Foundation/EnvTest.php
+++ tests/Foundation/EnvTest.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Testbench\Tests\Foundation;
 
+use Orchestra\Testbench\Attributes\WithEnv;
 use Orchestra\Testbench\Foundation\Env;
 use Orchestra\Testbench\TestCase;
 use PHPUnit\Framework\Attributes\Group;
