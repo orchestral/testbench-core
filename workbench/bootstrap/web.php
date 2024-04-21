cfaa49ea554b08544333dda45da67b0a31ee2581
Mior Muhammad Zaki <crynobone@gmail.com>
2024-04-21 16:07:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/bootstrap/web.php
+++ workbench/bootstrap/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use function Illuminate\Filesystem\join_paths;
-use function Orchestra\Testbench\workbench_path;
-
 Route::get('/dashboard', function () {
     return 'workbench::dashboard';
 })->middleware(['auth'])->name('dashboard');
