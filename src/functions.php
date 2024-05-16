64465e4bc6a456d24b03f23f346e3a866bcabef0
Mior Muhammad Zaki <crynobone@gmail.com>
2024-05-16 11:59:20 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/functions.php
+++ src/functions.php
@@ -264,9 +264,13 @@ function default_skeleton_path(string $path = ''): string
  */
 function package_path(string $path = ''): string
 {
-    $workingPath = \defined('TESTBENCH_WORKING_PATH')
-        ? TESTBENCH_WORKING_PATH
-        : getcwd();
+    $workingPath = getcwd();
+
+    if (\defined('TESTBENCH_WORKING_PATH')) {
+        $workingPath = TESTBENCH_WORKING_PATH;
+    } elseif (Env::has('TESTBENCH_WORKING_PATH')) {
+        $workingPath = Env::get('TESTBENCH_WORKING_PATH');
+    }
 
     if (Str::startsWith($path, './')) {
         return transform_relative_path($path, $workingPath);
