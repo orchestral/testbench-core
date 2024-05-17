00e984ebbe2f446f90bf64c9be11b6ca8270f5bd
Mior Muhammad Zaki <crynobone@gmail.com>
2024-05-17 09:40:32 +0800

    Use `getenv()` instead of `Env::get()` before booting the app (#207)
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -37,8 +37,8 @@ $createApp = static function (string $workingPath) {
     );
 };
 
-if (! defined('TESTBENCH_WORKING_PATH') && ! is_null(Env::get('TESTBENCH_WORKING_PATH'))) {
-    define('TESTBENCH_WORKING_PATH', Env::get('TESTBENCH_WORKING_PATH'));
+if (! defined('TESTBENCH_WORKING_PATH') && is_string(getenv('TESTBENCH_WORKING_PATH'))) {
+    define('TESTBENCH_WORKING_PATH', getenv('TESTBENCH_WORKING_PATH'));
 }
 
 $app = $createApp(realpath(__DIR__.'/../'));
