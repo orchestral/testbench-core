ddb276cfd86d55c7ae56616230b9dcbec06c1ec2
Mior Muhammad Zaki <crynobone@gmail.com>
2024-06-06 09:24:10 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integrations/CommandTest.php
+++ tests/Integrations/CommandTest.php
@@ -2,20 +2,12 @@
 
 namespace Orchestra\Testbench\Tests\Integrations;
 
-use Illuminate\Console\Application as Artisan;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase;
-use Workbench\App\Console\Commands\DummyCommand;
 
 class CommandTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        Artisan::starting(function ($artisan) {
-            $artisan->resolveCommands([DummyCommand::class]);
-        });
-
-        parent::setUp();
-    }
+    use WithWorkbench;
 
     /** @test */
     public function it_can_show_expected_output()
