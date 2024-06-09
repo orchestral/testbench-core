9179fa21da574adada8a0b497ad6060abd706151
Mior Muhammad Zaki <crynobone@gmail.com>
2024-06-09 21:51:00 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/UsesFrameworkConfiguration.php
+++ src/Attributes/UsesFrameworkConfiguration.php
@@ -3,9 +3,20 @@
 namespace Orchestra\Testbench\Attributes;
 
 use Attribute;
+use Illuminate\Foundation\Bootstrap\LoadConfiguration;
+use Orchestra\Testbench\Contracts\Attributes\Invokable as InvokableContract;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
-final class UsesFrameworkConfiguration
+final class UsesFrameworkConfiguration implements InvokableContract
 {
-    //
+    /**
+     * Handle the attribute.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    public function __invoke($app): void
+    {
+        $app->bind(LoadConfiguration::class, LoadConfiguration::class);
+    }
 }
