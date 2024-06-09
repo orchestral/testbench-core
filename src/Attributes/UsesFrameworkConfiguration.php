7110f45d3a4d7b7e79063befe4bdcffb3829a939
Mior Muhammad Zaki <crynobone@gmail.com>
2024-06-09 21:32:58 +0800

    Add `UsesFrameworkConfiguration` and `ResolvesLaravel` attributes.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Attributes/UsesFrameworkConfiguration.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Orchestra\Testbench\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+final class UsesFrameworkConfiguration
+{
+    //
+}
