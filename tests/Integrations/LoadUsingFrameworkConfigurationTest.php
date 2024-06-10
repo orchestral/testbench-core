3331ac1a6432d16fcec873f298e15781a9e5e43c
Mior Muhammad Zaki <crynobone@gmail.com>
2024-06-10 17:23:53 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integrations/LoadUsingFrameworkConfigurationTest.php
+++ tests/Integrations/LoadUsingFrameworkConfigurationTest.php
@@ -3,6 +3,7 @@
 namespace Orchestra\Testbench\Tests\Integrations;
 
 use Illuminate\Foundation\Bootstrap\LoadConfiguration;
+use Orchestra\Testbench\Attributes\ResolvesLaravel;
 use Orchestra\Testbench\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -10,17 +11,15 @@ use function Orchestra\Testbench\package_path;
 
 class LoadUsingFrameworkConfigurationTest extends TestCase
 {
-    #[\Override]
-    protected function resolveApplicationConfiguration($app)
+    protected function overrideLaravelConfiguration($app)
     {
         $app->instance(LoadConfiguration::class, new LoadConfiguration());
 
         $app->useConfigPath(package_path(['vendor', 'laravel', 'framework', 'config-stubs']));
-
-        parent::resolveApplicationConfiguration($app);
     }
 
     #[Test]
+    #[ResolvesLaravel('overrideLaravelConfiguration')]
     public function it_can_load_using_laravel_configurations()
     {
         $this->assertSame(LoadConfiguration::class, \get_class($this->app[LoadConfiguration::class]));
