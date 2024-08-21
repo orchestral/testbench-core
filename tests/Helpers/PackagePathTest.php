20a27d2beaa4b9b34a414861906a4d446526bafb
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-21 15:00:48 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Helpers/PackagePathTest.php
+++ tests/Helpers/PackagePathTest.php
@@ -6,36 +6,39 @@ use Orchestra\Testbench\TestCase;
 
 use function Orchestra\Testbench\package_path;
 
+/**
+ * @group workbench
+ */
 class PackagePathTest extends TestCase
 {
-    /**
-     * @test
-     *
-     * @group workbench
-     */
+    /** @test */
     public function it_can_use_package_path()
     {
         $this->assertSame(realpath(__DIR__.'/../../'), package_path());
         $this->assertSame(implode('', [realpath(__DIR__.'/../../'), DIRECTORY_SEPARATOR]), package_path(DIRECTORY_SEPARATOR));
+    }
 
+    /**
+     * @test
+     *
+     * @dataProvider pathDataProvider
+     */
+    public function it_can_resolve_correct_package_path(string $path)
+    {
         $this->assertSame(
-            realpath(__DIR__.DIRECTORY_SEPARATOR.'PackagePathTest.php'),
-            package_path('./tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')
+            realpath(__DIR__.DIRECTORY_SEPARATOR.'PackagePathTest.php'), $path
         );
+    }
 
-        $this->assertSame(
-            realpath(__DIR__.DIRECTORY_SEPARATOR.'PackagePathTest.php'),
-            package_path('./tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')
-        );
+    public static function pathDataProvider()
+    {
+        yield [package_path('tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')];
+        yield [package_path('./tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')];
+        yield [package_path(DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')];
 
-        $this->assertSame(
-            realpath(__DIR__.DIRECTORY_SEPARATOR.'PackagePathTest.php'),
-            package_path('tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')
-        );
-
-        $this->assertSame(
-            realpath(__DIR__.DIRECTORY_SEPARATOR.'PackagePathTest.php'),
-            package_path(DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'Helpers'.DIRECTORY_SEPARATOR.'PackagePathTest.php')
-        );
+        yield [package_path('tests', 'Helpers', 'PackagePathTest.php')];
+        yield [package_path(['tests', 'Helpers', 'PackagePathTest.php'])];
+        yield [package_path('./tests', 'Helpers', 'PackagePathTest.php')];
+        yield [package_path(['./tests', 'Helpers', 'PackagePathTest.php'])];
     }
 }
