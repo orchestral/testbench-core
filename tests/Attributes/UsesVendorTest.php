cebbfb0ac903b6aecd17ec1710efe61102aa3a4f
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-22 10:54:47 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Attributes/UsesVendorTest.php
+++ tests/Attributes/UsesVendorTest.php
@@ -5,13 +5,14 @@ namespace Orchestra\Testbench\Tests\Attributes;
 use Illuminate\Filesystem\Filesystem;
 use Orchestra\Testbench\Attributes\UsesVendor;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 use function Orchestra\Testbench\join_paths;
 use function Orchestra\Testbench\package_path;
 
 class UsesVendorTest extends TestCase
 {
-    /** @test */
+    #[Test]
     #[UsesVendor]
     public function it_can_uses_vendor_attribute()
     {
