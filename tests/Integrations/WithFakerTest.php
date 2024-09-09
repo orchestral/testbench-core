d1312a4b7fb23f8d9b501a3cc05c84095c9f0b8b
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-09 17:42:15 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integrations/WithFakerTest.php
+++ tests/Integrations/WithFakerTest.php
@@ -4,6 +4,7 @@ namespace Orchestra\Testbench\Tests\Integrations;
 
 use Faker\Generator;
 use Illuminate\Foundation\Testing\WithFaker;
+use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
 
 class WithFakerTest extends TestCase
@@ -14,5 +15,19 @@ class WithFakerTest extends TestCase
     public function it_can_use_faker()
     {
         $this->assertInstanceOf(Generator::class, $this->faker);
+    }
+
+    /**
+     * @requires PHP >= 8.0
+     * @test
+     */
+    #[WithConfig('app.faker_locale', 'it_IT')]
+     public function it_can_override_faker_locale()
+    {
+        $providerNames = array_map(function ($p) {
+            return get_class($p);
+        }, $this->faker()->getProviders());
+
+        $this->assertContains('Faker\Provider\it_IT\Person', $providerNames);
     }
 }
