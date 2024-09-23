d11bba5c1ff088073fe1112dd3ed7c065e902686
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-23 11:56:50 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/RequiresDatabase.php
+++ src/Attributes/RequiresDatabase.php
@@ -4,10 +4,12 @@ namespace Orchestra\Testbench\Attributes;
 
 use Attribute;
 use Closure;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use InvalidArgumentException;
 use Orchestra\Testbench\Contracts\Attributes\Actionable as ActionableContract;
 
-#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class RequiresDatabase implements ActionableContract
 {
     /**
@@ -18,12 +20,14 @@ final class RequiresDatabase implements ActionableContract
      * @param  bool  $default
      */
     public function __construct(
-        public string $driver,
+        public array|string $driver,
         public ?string $versionRequirement = null,
         public ?string $connection = null,
         public bool $default = true
     ) {
-        //
+        if (is_array($driver) && $default === true) {
+            throw new InvalidArgumentException('Unable to validate default connection when given an array of database drivers');
+        }
     }
 
     /**
@@ -37,12 +41,29 @@ final class RequiresDatabase implements ActionableContract
     {
         $connection = DB::connection($this->connection);
 
-        if ($connection->getDriverName() !== $this->driver) {
-            if ($this->default === true) {
-                \call_user_func($action, 'markTestSkipped', [\sprintf('Requires %s as the default database connection', $connection->getName())]);
-            } else {
-                \call_user_func($action, 'markTestSkipped', [\sprintf('Requires %s to use %s database connection', $connection->getName(), $this->driver)]);
+        if (
+            $this->default === true
+            && is_string($this->driver)
+            && $connection->getDriverName() !== $this->driver
+        ) {
+            \call_user_func($action, 'markTestSkipped', [\sprintf('Requires %s as the default database connection', $connection->getName())]);
+        }
+
+        $usingCorrectConnection = false;
+
+        foreach (Arr::wrap($this->driver) as $driver) {
+            if ($connection->getDriverName() === $driver) {
+                $usingCorrectConnection = true;
             }
+        }
+
+
+        if ($usingCorrectConnection === false) {
+            \call_user_func(
+                $action,
+                'markTestSkipped',
+                [\sprintf('Requires %s to use [%s] database connection', $connection->getName(), Arr::join($driver, ','))]
+            );
         }
 
         if (
