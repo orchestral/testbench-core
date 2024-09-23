8eb47389b797f1df8bc222e48b3df5431071b4f3
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-23 12:40:59 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Concerns/HandlesAssertionsTest.php
+++ tests/Concerns/HandlesAssertionsTest.php
@@ -10,7 +10,7 @@ class HandlesAssertionsTest extends TestCase
     use HandlesAssertions;
 
     /** @test */
-    public function it_should_mark_the_tests_as_skipped()
+    public function it_should_mark_the_tests_as_skipped_when_condition_is_true()
     {
         $this->markTestSkippedWhen(true, 'Successfully skipped current test');
 
@@ -18,10 +18,28 @@ class HandlesAssertionsTest extends TestCase
     }
 
     /** @test */
-    public function it_should_mark_the_tests_as_skipped_when_condition_()
+    public function it_should_mark_the_tests_as_skipped_when_condition_is_false()
     {
         $this->markTestSkippedWhen(function () {
             return false;
+        }, 'Failed skipped current test');
+
+        $this->assertTrue(true, 'Test is correctly executed.');
+    }
+
+    /** @test */
+    public function it_should_mark_the_tests_as_skipped_unless_condition_is_false()
+    {
+        $this->markTestSkippedUnless(false, 'Successfully skipped current test');
+
+        $this->assertTrue(false, 'Test incorrectly executed.');
+    }
+
+    /** @test */
+    public function it_should_mark_the_tests_as_skipped_unless_condition_is_true()
+    {
+        $this->markTestSkippedUnless(function () {
+            return true;
         }, 'Failed skipped current test');
 
         $this->assertTrue(true, 'Test is correctly executed.');
