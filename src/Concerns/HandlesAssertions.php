623ed4da64a71fe64dd7628a554f911bb50fe69d
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-23 12:26:50 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/HandlesAssertions.php
+++ src/Concerns/HandlesAssertions.php
@@ -5,9 +5,23 @@ namespace Orchestra\Testbench\Concerns;
 trait HandlesAssertions
 {
     /**
+     * Mark the test as skipped when condition is not equivalent to true.
+     *
+     * @param  (\Closure($this): bool)|bool|null  $condition
+     * @param  string  $message
+     * @return void
+     */
+    protected function markTestSkippedUnless($condition, string $message): void
+    {
+        if (!value($condition)) {
+            $this->markTestSkipped($message);
+        }
+    }
+
+    /**
      * Mark the test as skipped when condition is equivalent to true.
      *
-     * @param  (\Closure($this): mixed)|mixed|null  $condition
+     * @param  (\Closure($this): bool)|bool|null  $condition
      * @param  string  $message
      * @return void
      */
