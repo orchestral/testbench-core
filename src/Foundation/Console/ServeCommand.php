0dc4dc3cc178f10645736b3331fd4379103a1c0e
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-25 22:20:24 +0800

    [7.x] Allow configuring `PHP_CLI_SERVER_WORKERS` via Composer Script (#247)
    * [7.x] Allow configuring `PHP_CLI_SERVER_WORKERS` via Composer Script
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/ServeCommand.php
+++ src/Foundation/Console/ServeCommand.php
@@ -14,13 +14,7 @@ use function Orchestra\Testbench\package_path;
 
 class ServeCommand extends Command
 {
-    /**
-     * Execute the console command.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return int
-     */
+    /** {@inheritDoc} */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         if (
@@ -28,6 +22,12 @@ class ServeCommand extends Command
             && method_exists(ComposerConfig::class, 'disableProcessTimeout') // @phpstan-ignore function.impossibleType
         ) {
             ComposerConfig::disableProcessTimeout();
+        }
+
+        $workers = getenv('PHP_CLI_SERVER_WORKERS');
+
+        if (\is_string($workers) && filter_var($workers, FILTER_VALIDATE_INT) && ! isset($_ENV['PHP_CLI_SERVER_WORKERS'])) {
+            $_ENV['PHP_CLI_SERVER_WORKERS'] = (int) $workers;
         }
 
         $_ENV['TESTBENCH_WORKING_PATH'] = package_path();
@@ -41,12 +41,7 @@ class ServeCommand extends Command
         });
     }
 
-    /**
-     * Start a new server process.
-     *
-     * @param  bool  $hasEnvironment
-     * @return \Symfony\Component\Process\Process
-     */
+    /** {@inheritDoc} */
     protected function startProcess($hasEnvironment)
     {
         return tap(parent::startProcess($hasEnvironment), function (Process $process) {
@@ -62,12 +57,7 @@ class ServeCommand extends Command
         });
     }
 
-    /**
-     * Get the value of a command option.
-     *
-     * @param  string|null  $key
-     * @return string|array|bool|null
-     */
+    /** {@inheritDoc} */
     public function option($key = null)
     {
         $value = parent::option($key);
