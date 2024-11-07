820ce60c6806323c7a2ae6fc3df7b35b5b281793
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-07 20:27:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/RequiresEnv.php
+++ src/Attributes/RequiresEnv.php
@@ -6,7 +6,6 @@ use Attribute;
 use Closure;
 use Orchestra\Testbench\Contracts\Attributes\Actionable as ActionableContract;
 use Orchestra\Testbench\Foundation\Env;
-use Orchestra\Testbench\Foundation\UndefinedValue;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class RequiresEnv implements ActionableContract
@@ -33,11 +32,9 @@ final class RequiresEnv implements ActionableContract
      */
     public function handle($app, Closure $action): void
     {
-        $value = Env::get($this->key, new UndefinedValue);
-
         $message = $this->message ?? "Missing required environment variable `{$this->key}`";
 
-        if ($value instanceof UndefinedValue) {
+        if (Env::has($this->key) === false) {
             \call_user_func($action, 'markTestSkipped', [$message]);
         }
     }
