a2a18e223f2ea674ce5a62c857f9fde1abbececd
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-23 17:44:13 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -108,7 +108,7 @@ return [
     */
 
     'markdown' => [
-        'theme' => 'default',
+        'theme' => env('MAIL_MARKDOWN_THEME', 'default'),
 
         'paths' => [
             resource_path('views/vendor/mail'),
