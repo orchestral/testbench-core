725b655f415e4ace1a2b89c8d1770c6793923a9e
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-23 15:14:29 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/auth.php
+++ laravel/config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
-        'passwords' => 'users',
+        'guard' => env('AUTH_GUARD', 'web'),
+        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
     /*
