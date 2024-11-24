3bf1186269df864fe55454cd9279f59a882653bd
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-24 23:30:58 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/Commander.php
+++ src/Console/Commander.php
@@ -185,7 +185,7 @@ class Commander
      */
     public static function applicationBasePath()
     {
-        return static::$testbench::applicationBasePath();
+        return $_ENV['APP_BASE_PATH'] ?? static::$testbench::applicationBasePath();
     }
 
     /**
