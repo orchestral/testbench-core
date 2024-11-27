1b67ada4fdd623eec188fb07857823f583632120
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-27 11:22:04 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -15,7 +15,8 @@ use function Orchestra\Testbench\join_paths;
  */
 $createApp = static function (string $workingPath) {
     $config = Config::loadFromYaml(
-        defined('TESTBENCH_WORKING_PATH') ? TESTBENCH_WORKING_PATH : join_paths($workingPath, 'bootstrap', 'cache', 'testbench.yaml')
+        workingPath: defined('TESTBENCH_WORKING_PATH') ? TESTBENCH_WORKING_PATH : $workingPath,
+        filename: defined('TESTBENCH_WORKING_PATH') ? 'testbench.yaml' : join_paths($workingPath, 'bootstrap', 'cache', 'testbench.yaml')
     );
 
     $hasEnvironmentFile = ! is_null($config['laravel'])
