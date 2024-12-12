c26ef0a89a5dc4b1f7345cb2175af6b5bb67fc35
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-12 09:56:37 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/VendorPublishCommand.php
+++ src/Foundation/Console/VendorPublishCommand.php
@@ -4,7 +4,7 @@ namespace Orchestra\Testbench\Foundation\Console;
 
 use Illuminate\Foundation\Console\VendorPublishCommand as Command;
 
-use function Orchestra\Testbench\package_path;
+use function Orchestra\Testbench\transform_realpath_to_relative;
 
 /**
  * @codeCoverageIgnore
@@ -15,37 +15,19 @@ class VendorPublishCommand extends Command
     #[\Override]
     protected function status($from, $to, $type)
     {
-        $laravelPath = base_path();
-        $packagePath = package_path();
-
-        $pathLocation = function ($path) use ($laravelPath, $packagePath) {
-            $path = (string) realpath($path);
-
+        $format = function ($path) use ($type) {
             return match (true) {
-                str_starts_with($path, $laravelPath) => str_replace("{$laravelPath}/", '@laravel/', $path),
-                str_starts_with($path, $packagePath) => str_replace("{$packagePath}/", './', $path),
-                default => $path,
+                $type === 'directory' && is_link($path) => $path,
+                $this->files->exists($path) => $path,
+                default => (string) realpath($path),
             };
         };
-
-        $fromLocation = $pathLocation($from);
-        $toLocation = $pathLocation($to);
-
-        if (
-            $type === 'directory' &&
-            $fromLocation === $toLocation &&
-            is_link($to)
-        ) {
-            $this->components->task('Synced directory');
-
-            return;
-        }
 
         $this->components->task(\sprintf(
             'Copying %s [%s] to [%s]',
             $type,
-            $fromLocation,
-            $toLocation,
+            transform_realpath_to_relative($format($from)),
+            transform_realpath_to_relative($format($to)),
         ));
     }
 }
