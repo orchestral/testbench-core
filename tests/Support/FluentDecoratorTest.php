3a7c8b3255b4615153fd690ec3e426c0b5b09dc6
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-15 17:29:55 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Support/FluentDecoratorTest.php
+++ tests/Support/FluentDecoratorTest.php
@@ -11,10 +11,8 @@ class FluentDecoratorTest extends TestCase
     /** @test */
     public function it_can_be_utilise_fluent_features()
     {
-        $fluent = new class($attributes = [
-            'testbench' => true,
-            'class' => __CLASS__,
-        ]) extends FluentDecorator {
+        $fluent = new class($attributes = ['testbench' => true, 'class' => __CLASS__]) extends FluentDecorator
+        {
             // ...
         };
 
