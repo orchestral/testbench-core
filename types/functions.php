b0365ffccbaa04361571280f58923b13795eceeb
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-29 17:29:39 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- types/functions.php
+++ types/functions.php
@@ -1,6 +1,10 @@
 <?php
 
 use function Orchestra\Testbench\default_skeleton_path;
+use function Orchestra\Testbench\laravel_version_compare;
+use function Orchestra\Testbench\package_path;
+use function Orchestra\Testbench\phpunit_version_compare;
+use function Orchestra\Testbench\workbench_path;
 use function PHPStan\Testing\assertType;
 
 assertType('string', default_skeleton_path());
@@ -8,26 +12,18 @@ assertType('string', default_skeleton_path('app'));
 assertType('string', default_skeleton_path('app', '.gitkeep'));
 assertType('string', default_skeleton_path(['app', '.gitkeep']));
 
-use function Orchestra\Testbench\package_path;
-
 assertType('string', package_path());
 assertType('string', package_path('laravel'));
 assertType('string', package_path('laravel', 'app', '.gitkeep'));
 assertType('string', package_path(['laravel', 'app', '.gitkeep']));
-
-use function Orchestra\Testbench\workbench_path;
 
 assertType('string', workbench_path());
 assertType('string', workbench_path('app'));
 assertType('string', workbench_path('app', 'Providers'));
 assertType('string', workbench_path(['app', 'Providers']));
 
-use function Orchestra\Testbench\laravel_version_compare;
-
 assertType('bool', laravel_version_compare('7.0.0', '>='));
 assertType('int', laravel_version_compare('7.0.0'));
-
-use function Orchestra\Testbench\phpunit_version_compare;
 
 assertType('bool', phpunit_version_compare('9.0.0', '>='));
 assertType('int', phpunit_version_compare('9.0.0'));
