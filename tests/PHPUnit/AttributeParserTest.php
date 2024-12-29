8acaff936083f2c50f4e17a7a54382fdbab542b8
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-29 17:40:29 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/PHPUnit/AttributeParserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Orchestra\Testbench\Tests\PHPUnit;
+
+use Orchestra\Testbench\PHPUnit\AttributeParser;
+use PHPUnit\Framework\TestCase;
+
+class AttributeParserTest extends TestCase
+{
+    /** @test */
+    public function it_can_validate_attribute()
+    {
+        $this->assertFalse(AttributeParser::validAttribute(TestCase::class));
+        $this->assertFalse(AttributeParser::validAttribute('Orchestra\Testbench\Support\FluentDecorator'));
+
+        $this->assertTrue(AttributeParser::validAttribute('Orchestra\Testbench\Attributes\Define'));
+    }
+}
