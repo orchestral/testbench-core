029a46d0f445d219654692007059d621ea58b151
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-29 17:26:22 +0800

    [7.x] Types Improvements
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ types/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+use function PHPStan\Testing\assertType;
+
+use function Orchestra\Testbench\default_skeleton_path;
+
+assertType('string', default_skeleton_path());
+assertType('string', default_skeleton_path('app'));
+assertType('string', default_skeleton_path('app', '.gitkeep'));
+assertType('string', default_skeleton_path(['app', '.gitkeep']));
+
+
+use function Orchestra\Testbench\package_path;
+
+assertType('string', package_path());
+assertType('string', package_path('laravel'));
+assertType('string', package_path('laravel', 'app', '.gitkeep'));
+assertType('string', package_path(['laravel', 'app', '.gitkeep']));
+
+
+use function Orchestra\Testbench\workbench_path;
+
+assertType('string', workbench_path());
+assertType('string', workbench_path('app'));
+assertType('string', workbench_path('app', 'Providers'));
+assertType('string', workbench_path(['app', 'Providers']));
+
+use function Orchestra\Testbench\laravel_version_compare;
+
+assertType('bool', laravel_version_compare('7.0.0', '>='));
+assertType('int', laravel_version_compare('7.0.0'));
+
+use function Orchestra\Testbench\phpunit_version_compare;
+
+assertType('bool', phpunit_version_compare('9.0.0', '>='));
+assertType('int', phpunit_version_compare('9.0.0'));
