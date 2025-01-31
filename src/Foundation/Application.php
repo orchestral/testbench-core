0598b5a92ddf34ebe3337ee981a290e232b753d7
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-01 07:42:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -4,9 +4,9 @@ namespace Orchestra\Testbench\Foundation;
 
 use Illuminate\Console\Application as Artisan;
 use Illuminate\Console\Scheduling\ScheduleListCommand;
-use Illuminate\Foundation\Bootstrap\HandleExceptions;
 use Illuminate\Console\Signals;
 use Illuminate\Database\Schema\Builder as SchemaBuilder;
+use Illuminate\Foundation\Bootstrap\HandleExceptions;
 use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Illuminate\Foundation\Bootstrap\RegisterProviders;
 use Illuminate\Foundation\Console\AboutCommand;
@@ -211,7 +211,7 @@ class Application
         ThrottleRequests::shouldHashKeys();
         SchemaBuilder::$defaultStringLength = 255;
         SchemaBuilder::$defaultMorphKeyType = 'int';
-        Signals::resolveAvailabilityUsing(null);
+        Signals::resolveAvailabilityUsing(null); // @phpstan-ignore argument-type
         TrimStrings::flushState();
         TrustProxies::flushState();
         VerifyCsrfToken::flushState();
