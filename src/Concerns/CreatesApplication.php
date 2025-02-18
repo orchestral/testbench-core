3f8eb2608ce8876252ae86f9b21da350f17f1b94
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-19 06:29:55 +0800

    Readd `getBasePath()` as deprecated.
    fixes #311
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/CreatesApplication.php
+++ src/Concerns/CreatesApplication.php
@@ -226,9 +226,21 @@ trait CreatesApplication
      */
     protected function getApplicationBasePath()
     {
-        return method_exists($this, 'getBasePath')
-            ? $this->getBasePath()
-            : static::applicationBasePath();
+        return $this->getBasePath() ?? static::applicationBasePath();
+    }
+
+    /**
+     * Resolve the application's base path (deprecated).
+     *
+     * @api
+     *
+     * @return string|null
+     *
+     * @deprecated 7.53.0 Use `getApplicationBasePath()` instead.
+     */
+    protected function getBasePath()
+    {
+        return null;
     }
 
     /**
