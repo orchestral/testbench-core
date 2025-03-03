181518c50bfe92d5cce395197d0c9db423b34704
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-03 22:25:47 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/config/filesystems.php
+++ laravel/config/filesystems.php
@@ -32,8 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
+            'root' => storage_path('app'),
             'throw' => false,
             'report' => false,
         ],
