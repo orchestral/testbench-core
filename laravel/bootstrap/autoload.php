6aa457914f914b6b6f0459a8aeb5f859fa7648ee
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-04 07:19:36 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- laravel/bootstrap/autoload.php
+++ laravel/bootstrap/autoload.php
@@ -1,11 +1,5 @@
 <?php
 
-if (! file_exists(__DIR__.'/../vendor/autoload.php')) {
-    require_once __DIR__.'/../vendor/autoload.php';
-
-    return;
-}
-
 $workingPath = match (true) {
     defined('TESTBENCH_WORKING_PATH') => TESTBENCH_WORKING_PATH,
     is_string(getenv('TESTBENCH_WORKING_PATH')) => getenv('TESTBENCH_WORKING_PATH'),
