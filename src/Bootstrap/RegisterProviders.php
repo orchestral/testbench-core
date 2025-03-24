b2dfc3d53cf9a130a4049d0670e47b5ab41a563d
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-24 22:42:02 +0800

    [7.x] Add support for `workbench/bootstrap/providers.php`
    solves #328
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Bootstrap/RegisterProviders.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Orchestra\Testbench\Bootstrap;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class RegisterProviders extends \Illuminate\Foundation\Bootstrap\RegisterProviders
+{
+    /**
+     * The service providers that should be merged before registration.
+     *
+     * @var array
+     */
+    protected static $merge = [];
+
+    /**
+     * The path to the bootstrap provider configuration file.
+     *
+     * @var string|null
+     */
+    protected static $bootstrapProviderPath;
+
+    /** {@inheritDoc} */
+    #[\Override]
+    public function bootstrap(Application $app)
+    {
+        $this->mergeAdditionalProviders($app);
+
+        parent::bootstrap($app);
+    }
+
+    /**
+     * Merge the additional configured providers into the configuration.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    protected function mergeAdditionalProviders(Application $app): void
+    {
+        if (static::$bootstrapProviderPath &&
+            file_exists(static::$bootstrapProviderPath)) {
+            $packageProviders = require static::$bootstrapProviderPath;
+
+            foreach ($packageProviders as $index => $provider) {
+                if (! class_exists($provider)) {
+                    unset($packageProviders[$index]);
+                }
+            }
+        }
+
+        $app->make('config')->set(
+            'app.providers',
+            array_merge(
+                $app->make('config')->get('app.providers'),
+                static::$merge,
+                array_values($packageProviders ?? []),
+            ),
+        );
+    }
+
+    /**
+     * Merge the given providers into the provider configuration before registration.
+     *
+     * @param  array  $providers
+     * @param  string|null  $bootstrapProviderPath
+     * @return void
+     */
+    public static function merge(array $providers, ?string $bootstrapProviderPath = null): void
+    {
+        static::$bootstrapProviderPath = $bootstrapProviderPath;
+
+        static::$merge = array_values(array_filter(array_unique(
+            array_merge(static::$merge, $providers)
+        )));
+    }
+
+    /**
+     * Flush the bootstrapper's global state.
+     *
+     * @return void
+     */
+    public static function flushState(): void
+    {
+        static::$bootstrapProviderPath = null;
+
+        static::$merge = [];
+    }
+}
--- src/Concerns/CreatesApplication.php
+++ src/Concerns/CreatesApplication.php
@@ -19,6 +19,7 @@ use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\Attributes\WithEnv;
 use Orchestra\Testbench\Attributes\WithImmutableDates;
 use Orchestra\Testbench\Bootstrap\LoadEnvironmentVariables;
+use Orchestra\Testbench\Bootstrap\RegisterProviders;
 use Orchestra\Testbench\Features\TestingFeature;
 use Orchestra\Testbench\Foundation\PackageManifest;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
@@ -36,6 +37,7 @@ use function Orchestra\Testbench\refresh_router_lookups;
 trait CreatesApplication
 {
     use InteractsWithWorkbench;
+    use WithLaravelBootstrapFile;
 
     /**
      * Get the application's base path.
@@ -385,6 +387,10 @@ trait CreatesApplication
                 });
             }
 
+            if (\is_string($bootstrapProviderPath = $this->getApplicationBootstrapFile('providers.php'))) {
+                RegisterProviders::merge([], $bootstrapProviderPath);
+            }
+
             $config->set([
                 'app.aliases' => $this->resolveApplicationAliases($app),
                 'app.providers' => $this->resolveApplicationProviders($app),
@@ -461,7 +467,7 @@ trait CreatesApplication
 
         $app->make('Illuminate\Foundation\Bootstrap\RegisterFacades')->bootstrap($app);
         $app->make('Illuminate\Foundation\Bootstrap\SetRequestForConsole')->bootstrap($app);
-        $app->make('Illuminate\Foundation\Bootstrap\RegisterProviders')->bootstrap($app);
+        $app->make(RegisterProviders::class)->bootstrap($app);
 
         if (class_exists('Illuminate\Database\Eloquent\LegacyFactoryServiceProvider')) {
             $app->register('Illuminate\Database\Eloquent\LegacyFactoryServiceProvider');
