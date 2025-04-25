b1613c9822719326d4c4ad16851032f40962b1c6
Mior Muhammad Zaki <crynobone@gmail.com>
2025-04-25 15:06:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -35,6 +35,7 @@ use Orchestra\Testbench\Concerns\CreatesApplication;
 use Orchestra\Testbench\Console\Commander;
 use Orchestra\Testbench\Contracts\Config as ConfigContract;
 use Orchestra\Testbench\Workbench\Workbench;
+
 use function Orchestra\Sidekick\join_paths;
 
 /**
