74f205b2b98ac12966cbfa96788587e02495323b
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-07 07:16:58 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Workbench/Workbench.php
+++ src/Workbench/Workbench.php
@@ -319,7 +319,7 @@ class Workbench
      */
     public static function applicationUserModel(): ?string
     {
-        if (! isset(static::$cachedUserModel)) {
+        if (\is_null(static::$cachedUserModel)) {
             /** @var class-string<\Illuminate\Foundation\Auth\User>|false $userModel */
             $userModel = match (true) {
                 Env::has('AUTH_MODEL') => Env::get('AUTH_MODEL'),
