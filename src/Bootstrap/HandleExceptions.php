6be9c449b8418672473195d285702aa010935ccf
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-12 13:15:30 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Bootstrap/HandleExceptions.php
+++ src/Bootstrap/HandleExceptions.php
@@ -5,7 +5,7 @@ namespace Orchestra\Testbench\Bootstrap;
 use Illuminate\Log\LogManager;
 use Orchestra\Testbench\Exceptions\DeprecatedException;
 use Orchestra\Testbench\Foundation\Env;
-use Throwable;
+use PHPUnit\Runner\ErrorHandler;
 
 use function Orchestra\Sidekick\join_paths;
 
@@ -78,5 +78,58 @@ final class HandleExceptions extends \Illuminate\Foundation\Bootstrap\HandleExce
         return ! class_exists(LogManager::class)
             || ! self::$app->hasBeenBootstrapped()
             || ! Env::get('LOG_DEPRECATIONS_WHILE_TESTING', true);
+    }
+
+    /** {@inheritDoc} */
+    #[\Override]
+    public static function forgetApp()
+    {
+        if (\is_null(self::$app)) {
+            return;
+        }
+
+        self::flushHandlersState();
+
+        self::$app = null; /** @phpstan-ignore assign.propertyType */
+        self::$reservedMemory = null;
+    }
+
+    /**
+     * Flush the bootstrapper's global handlers state.
+     *
+     * @return void
+     */
+    public static function flushHandlersState()
+    {
+        while (true) {
+            $previousHandler = set_exception_handler(static fn () => null);
+            restore_exception_handler();
+
+            if ($previousHandler === null) {
+                break;
+            }
+
+            restore_exception_handler();
+        }
+
+        while (true) {
+            $previousHandler = set_error_handler(static fn () => null); /** @phpstan-ignore argument.type */
+            restore_error_handler();
+
+            if ($previousHandler === null) {
+                break;
+            }
+
+            restore_error_handler();
+        }
+
+        if (class_exists(ErrorHandler::class)) {
+            $instance = ErrorHandler::instance();
+
+            if ((fn () => $this->enabled ?? false)->call($instance)) {
+                $instance->disable();
+                $instance->enable();
+            }
+        }
     }
 }
