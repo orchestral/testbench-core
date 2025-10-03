520ce54554205268db0cd4656c65408692945603
Mior Muhammad Zaki <crynobone@gmail.com>
2025-10-03 09:34:39 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/functions.php
+++ src/functions.php
@@ -69,7 +69,7 @@ function remote(array|string $command, array|string $env = [], ?bool $tty = null
         package_path(), $env, $tty
     );
 
-    $binary = \defined('TESTBENCH_DUSK') ? 'testbench-dusk' : 'testbench';
+    $binary = Sidekick\is_testbench_cli(dusk: true) ? 'testbench-dusk' : 'testbench';
 
     $commander = is_file($vendorBinary = package_path('vendor', 'bin', $binary))
         ? $vendorBinary
