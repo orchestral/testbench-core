e5c2f488a5adbac074c8d1744242636d38203a61
Mior Muhammad Zaki <crynobone@gmail.com>
2025-10-30 19:18:35 +0800

    [10.x] Supports flushing `JsonResource` and `JsonApiResource` states.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -22,6 +22,7 @@ use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Http\Middleware\TrustHosts;
 use Illuminate\Http\Middleware\TrustProxies;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\JsonApi\JsonApiResource;
 use Illuminate\Mail\Markdown;
 use Illuminate\Queue\Console\WorkCommand;
 use Illuminate\Queue\Queue;
@@ -233,7 +234,13 @@ class Application
             HandleExceptions::flushState($instance);
         }
 
-        JsonResource::wrap('data');
+        if (class_exists(JsonApiResource::class)) {
+            JsonResource::flushState();
+            JsonApiResource::flushState();
+        } else {
+            JsonResource::wrap('data');
+        }
+
         Markdown::flushState();
         Migrator::withoutMigrations([]);
         Model::handleDiscardedAttributeViolationUsing(null);
