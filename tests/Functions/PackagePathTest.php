2a02ceecc341a6c65392663fa17b22452bb67078
Mior Muhammad Zaki <crynobone@gmail.com>
2025-12-01 06:14:30 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Functions/PackagePathTest.php
+++ tests/Functions/PackagePathTest.php
@@ -28,22 +28,22 @@ class PackagePathTest extends TestCase
     {
         $this->assertSame(
             realpath(join_paths(__DIR__, 'PackagePathTest.php')),
-            package_path(join_paths('./tests', 'Helpers', 'PackagePathTest.php'))
+            package_path(join_paths('./tests', 'Functions', 'PackagePathTest.php'))
         );
 
         $this->assertSame(
             realpath(join_paths(__DIR__, 'PackagePathTest.php')),
-            package_path(join_paths('tests', 'Helpers', 'PackagePathTest.php'))
+            package_path(join_paths('tests', 'Functions', 'PackagePathTest.php'))
         );
 
         $this->assertSame(
             realpath(join_paths(__DIR__, 'PackagePathTest.php')),
-            package_path(DIRECTORY_SEPARATOR.join_paths('tests', 'Helpers', 'PackagePathTest.php'))
+            package_path(DIRECTORY_SEPARATOR.join_paths('tests', 'Functions', 'PackagePathTest.php'))
         );
 
         $this->assertSame(
             realpath(join_paths(__DIR__, 'PackagePathTest.php')),
-            package_path(join_paths('tests', 'Helpers', 'PackagePathTest.php'))
+            package_path(join_paths('tests', 'Functions', 'PackagePathTest.php'))
         );
     }
 
