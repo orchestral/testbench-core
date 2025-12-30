fa36f40f0a5620626da2b60ff35c3314bba9bca6
Mior Muhammad Zaki <crynobone@gmail.com>
2025-12-30 15:52:06 +0800

    [7.x] Add `WithFixtures` to automatically loads fixtures file for test (#379)
    * [7.x] Add `WithFixtures` to automatically loads fixtures file for test
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * formatting
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * update `orchestra/sidekick`
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Concerns/WithFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Orchestra\Testbench\Concerns;
+
+use Illuminate\Support\Str;
+
+use function Orchestra\Sidekick\Filesystem\filename_from_classname;
+
+/**
+ * @api
+ *
+ * @codeCoverageIgnore
+ */
+trait WithFixtures
+{
+    /**
+     * Setup test case to include fixture file using ".fixtures.php" suffix if it's available.
+     *
+     * @return void
+     */
+    protected static function setupWithFixturesForTestingEnvironment(): void
+    {
+        $classFileName = filename_from_classname(static::class);
+
+        if ($classFileName === false) {
+            return;
+        }
+
+        if (! is_file($fixtureFileName = Str::replaceLast('.php', '.fixtures.php', $classFileName))) {
+            return;
+        }
+
+        require_once $fixtureFileName;
+    }
+}
