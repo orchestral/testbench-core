0c24a0a99ac412c37688f14f9bf8945dd8587006
Mior Muhammad Zaki <crynobone@gmail.com>
2026-02-20 08:51:49 +0800

    Fix `#[UsesVendor]` attribute fails due to unbooted application, causing container `BindingResolutionException`
    fix #372
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Attributes/UsesVendor.php
+++ src/Attributes/UsesVendor.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Facade;
 use Orchestra\Testbench\Contracts\Attributes\AfterEach as AfterEachContract;
 use Orchestra\Testbench\Contracts\Attributes\BeforeEach as BeforeEachContract;
+use Orchestra\Testbench\Foundation\Actions\CreateVendorSymlink;
 use Orchestra\Testbench\Foundation\Actions\DeleteVendorSymlink;
 use Orchestra\Testbench\Foundation\Application as Testbench;
 
@@ -28,14 +29,11 @@ final class UsesVendor implements AfterEachContract, BeforeEachContract
      */
     public function beforeEach($app): void
     {
-        $laravel = Testbench::createVendorSymlink(base_path(), package_path('vendor'));
+        $laravel = clone $app;
+
+        (new CreateVendorSymlink(package_path('vendor')))->handle($laravel);
 
         $this->vendorSymlinkCreated = $laravel['TESTBENCH_VENDOR_SYMLINK'] ?? false;
-
-        Facade::clearResolvedInstances();
-        Facade::setFacadeApplication($app);
-
-        Application::setInstance($app);
     }
 
     /**
