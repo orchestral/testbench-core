5ae6f0c737486299c1b71dd11d3db0020ec3a207
Mior Muhammad Zaki <crynobone@gmail.com>
2026-03-17 06:58:03 +0800

    Use `require` instead of `require_once` when executed via paratest
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/WithFixtures.php
+++ src/Concerns/WithFixtures.php
@@ -3,6 +3,7 @@
 namespace Orchestra\Testbench\Concerns;
 
 use Illuminate\Support\Str;
+use Orchestra\Sidekick\Env;
 
 use function Orchestra\Sidekick\Filesystem\filename_from_classname;
 
@@ -30,6 +31,10 @@ trait WithFixtures
             return;
         }
 
-        require_once $fixtureFileName;
+        if (Env::has('TEST_TOKEN')) {
+            require $fixtureFileName;
+        } else {
+            require_once $fixtureFileName;
+        }
     }
 }
