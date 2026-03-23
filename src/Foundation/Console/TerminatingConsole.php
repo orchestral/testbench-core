f2392956bcdc9579714383a502d918a28789c6c2
Mior Muhammad Zaki <crynobone@gmail.com>
2026-03-24 06:30:27 +0800

    Utilise `TerminatingConsole` on `testbench serve`
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/TerminatingConsole.php
+++ src/Foundation/Console/TerminatingConsole.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Testbench\Foundation\Console;
 
+use Closure;
 use Illuminate\Support\Collection;
 
 /**
@@ -12,17 +13,17 @@ final class TerminatingConsole
     /**
      * The terminating callbacks.
      *
-     * @var array<int, (callable():void)>
+     * @var array<int, (callable(?int):void)>
      */
     protected static array $beforeTerminatingCallbacks = [];
 
     /**
      * Register a callback to be run before terminating the command.
      *
-     * @param  callable():void  $callback
+     * @param  callable(?int):void  $callback
      * @return void
      */
-    public static function before(callable $callback): void
+    public static function before(Closure|callable $callback): void
     {
         array_unshift(self::$beforeTerminatingCallbacks, $callback);
     }
@@ -31,7 +32,7 @@ final class TerminatingConsole
      * Register a callback to be run before terminating the command.
      *
      * @param  bool  $condition
-     * @param  callable():void  $callback
+     * @param  callable(?int):void  $callback
      * @return void
      */
     public static function beforeWhen(bool $condition, callable $callback): void
@@ -44,13 +45,14 @@ final class TerminatingConsole
     /**
      * Handle terminating console.
      *
+     * @param  ?int  $signal
      * @return void
      */
-    public static function handle(): void
+    public static function handle(?int $signal = null): void
     {
         (new Collection(self::$beforeTerminatingCallbacks))
-            ->each(static function ($callback) {
-                \call_user_func($callback);
+            ->each(static function ($callback) use ($signal) {
+                \call_user_func($callback, $signal);
             });
 
         self::flush();
