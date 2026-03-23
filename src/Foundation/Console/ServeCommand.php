b6888e81b639c4f54a0a409d1a5d615404e9020d
Mior Muhammad Zaki <crynobone@gmail.com>
2026-03-24 06:01:19 +0800

    Add `TESTBENCH_USER_MODEL` environment variable (#397)
    * Add `TESTBENCH_USER_MODEL` environment variable.
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * formatting
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/ServeCommand.php
+++ src/Foundation/Console/ServeCommand.php
@@ -3,9 +3,11 @@
 namespace Orchestra\Testbench\Foundation\Console;
 
 use Composer\Config as ComposerConfig;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Console\ServeCommand as Command;
 use Orchestra\Testbench\Foundation\Events\ServeCommandEnded;
 use Orchestra\Testbench\Foundation\Events\ServeCommandStarted;
+use Orchestra\Testbench\Workbench\Workbench;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
@@ -34,15 +36,28 @@ class ServeCommand extends Command
             $_ENV['PHP_CLI_SERVER_WORKERS'] = (int) $workers;
         }
 
-        $_ENV['TESTBENCH_WORKING_PATH'] = package_path();
-
-        static::$passthroughVariables[] = 'TESTBENCH_WORKING_PATH';
+        $this->addPassThroughEnvironmentVariable('TESTBENCH_WORKING_PATH', package_path());
+        $this->addPassThroughEnvironmentVariable('TESTBENCH_USER_MODEL', Workbench::applicationUserModel() ?? User::class);
 
         event(new ServeCommandStarted($input, $output, $this->components));
 
         return tap(parent::execute($input, $output), function ($exitCode) use ($input, $output) {
             event(new ServeCommandEnded($input, $output, $this->components, $exitCode));
         });
+    }
+
+    /**
+     * Add an environment variable that should be passed through to the server process.
+     *
+     * @param  string  $name
+     * @param  mixed  $value
+     * @return void
+     */
+    protected function addPassThroughEnvironmentVariable(string $name, mixed $value): void
+    {
+        $_ENV[$name] = $value;
+
+        static::$passthroughVariables[] = $name;
     }
 
     /** {@inheritDoc} */
