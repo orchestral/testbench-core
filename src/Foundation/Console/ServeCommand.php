1a6bcaabaa684ef631668c0351a384e5914d9e6d
Mior Muhammad Zaki <crynobone@gmail.com>
2026-03-30 17:34:32 +0800

    [8.x] Overrides `ServeCommand::trap()` method to use `TerminatingConsole`
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Console/ServeCommand.php
+++ src/Foundation/Console/ServeCommand.php
@@ -64,13 +64,14 @@ class ServeCommand extends Command
     #[\Override]
     protected function startProcess($hasEnvironment)
     {
-        return tap(parent::startProcess($hasEnvironment), function (Process $process) {
-            TerminatingConsole::before(static function ($signal) use ($process) {
-                if ($process->isRunning()) {
-                    $process->stop(10, $signal);
-                }
-            });
-        });
+        return parent::startProcess($hasEnvironment);
+    }
+
+    /** {@inhertiDoc} */
+    #[\Override]
+    public function trap($signals, $callback)
+    {
+        TerminatingConsole::before($callback);
     }
 
     /** {@inheritDoc} */
