eb299364fdc5bfb2c80aba38c28c2549cbb02538
Mior Muhammad Zaki <crynobone@gmail.com>
2026-04-22 13:42:21 +0800

    Revert "Refactor WithConfig to set config after resolving (#405)" (#406)
    This reverts commit 8398113d7edcf32cacb34013dfd98a53316cacb8.

--- src/Attributes/WithConfig.php
+++ src/Attributes/WithConfig.php
@@ -4,7 +4,6 @@ namespace Orchestra\Testbench\Attributes;
 
 use Attribute;
 use Orchestra\Testbench\Contracts\Attributes\Invokable as InvokableContract;
-use Illuminate\Contracts\Config\Repository;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class WithConfig implements InvokableContract
@@ -28,9 +27,9 @@ final class WithConfig implements InvokableContract
      */
     public function __invoke($app): void
     {
-        // This ensures all configuration keys are loaded before adding the values.
-        $app->afterResolving('config', function (Repository $config) {
-            $config->set($this->key, $this->value);
-        });
+        /** @var \Illuminate\Contracts\Config\Repository $config */
+        $config = $app->make('config');
+
+        $config->set($this->key, $this->value);
     }
 }
