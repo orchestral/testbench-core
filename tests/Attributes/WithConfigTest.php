457f990fdb994fc8f6706950e342bc0965e799bc
Mior Muhammad Zaki <crynobone@gmail.com>
2026-04-23 17:23:01 +0800

    Defers `WithConfig` attribute by default (#410)
    * Defers `WithConfig` attribute by default
    
    fix #407
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Attributes/WithConfigTest.php
+++ tests/Attributes/WithConfigTest.php
@@ -3,15 +3,57 @@
 namespace Orchestra\Testbench\Tests\Attributes;
 
 use Orchestra\Testbench\Attributes\WithConfig;
+use Orchestra\Testbench\Concerns\WithFixtures;
 use Orchestra\Testbench\Tests\TestCase;
 
 class WithConfigTest extends TestCase
 {
+    use WithFixtures;
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string>
+     */
+    protected function getPackageProviders($app)
+    {
+        return class_exists(WithConfigTest\WorkbenchServiceProvider::class, false)
+            ? [WithConfigTest\WorkbenchServiceProvider::class]
+            : [];
+    }
+
     /** @test */
     #[WithConfig('testbench.attribute', true)]
     public function it_can_resolve_defined_configuration()
     {
         $this->assertSame(true, config('testbench.attribute'));
+    }
+
+    /**
+     * @test
+     *
+     * @group without-parallel
+     */
+    #[WithConfig('testbench.session.attribute', true)]
+    public function it_can_deferred_resolve_defined_configuration()
+    {
+        $this->assertSame(true, config('testbench.session.attribute'));
+        $this->assertSame(false, config('testbench.session.report'));
+        $this->assertSame(1, config('testbench.api'));
+    }
+
+    /**
+     * @test
+     *
+     * @group without-parallel
+     */
+    #[WithConfig('testbench.session.attribute', true, defer: false)]
+    public function it_can_eagerly_resolve_defined_configuration()
+    {
+        $this->assertSame(true, config('testbench.session.attribute'));
+        $this->assertNull(config('testbench.session.report'));
+        $this->assertSame(1, config('testbench.api'));
     }
 
     /** @test */
