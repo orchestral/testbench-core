cb142ef0aff7f82092d50d4f4ab6ff81e955af4f
Mior Muhammad Zaki <crynobone@gmail.com>
2026-09-23 21:23:52 +0800

    Add `Orchestra\Testbench\php_version_compare()` function
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -13,15 +13,28 @@ class HelpersTest extends TestCase
     /** @test */
     public function it_can_compare_laravel_version()
     {
-        $this->assertSame(0, \Orchestra\Testbench\laravel_version_compare(Application::VERSION));
-        $this->assertTrue(\Orchestra\Testbench\laravel_version_compare(Application::VERSION, '=='));
+        $laravelVersion = \Orchestra\Sidekick\laravel_normalize_version();
+
+        $this->assertSame(0, \Orchestra\Testbench\laravel_version_compare($laravelVersion));
+        $this->assertTrue(\Orchestra\Testbench\laravel_version_compare($laravelVersion, '=='));
+    }
+
+    /** @test */
+    public function it_can_compare_php_version()
+    {
+        $phpVersion = \Orchestra\Sidekick\php_normalize_version();
+
+        $this->assertSame(0, \Orchestra\Testbench\php_version_compare($phpVersion));
+        $this->assertTrue(\Orchestra\Testbench\php_version_compare($phpVersion, '=='));
     }
 
     /** @test */
     public function it_can_compare_phpunit_version()
     {
-        $this->assertSame(0, \Orchestra\Testbench\phpunit_version_compare(Version::id()));
-        $this->assertTrue(\Orchestra\Testbench\phpunit_version_compare(Version::id(), '=='));
+        $phpunitVersion = \Orchestra\Sidekick\phpunit_normalize_version();
+
+        $this->assertSame(0, \Orchestra\Testbench\phpunit_version_compare($phpunitVersion));
+        $this->assertTrue(\Orchestra\Testbench\phpunit_version_compare($phpunitVersion, '=='));
     }
 
     /** @test */
